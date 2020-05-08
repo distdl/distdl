a1b35b51dac51530127f37b9095553facf5a6c1a
Russell J. Hewett <rjh@rjh.io>
2020-05-08 14:27:32 -0400

    chore(#3) corrected development status

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
