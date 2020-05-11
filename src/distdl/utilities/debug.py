06151bdc972ddd1f667ba6fef202ad70372335c9
Russell J. Hewett <rhewett@vt.edu>
2020-05-11 16:15:58 -0400

    feat(#12) Added global ordered print

---
+++ src/distdl/utilities/debug.py
@@ -0,0 +1,10 @@
+def print_sequential(comm, val):
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    if rank == 0:
+        print(f'{val}')
+        for i in range(1, size):
+            val = comm.recv(source=i, tag=0)
+            print(f'{val}')
+    else:
+        comm.send(val, dest=0, tag=0)
