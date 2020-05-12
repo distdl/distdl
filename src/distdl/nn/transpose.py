82428f6631db353fe9a5ef3fa2e44c1f3bfb8942
Russell J. Hewett <rhewett@vt.edu>
2020-05-12 14:12:53 -0400

    feat(#7) Added forward function for transpose layer.

--- src/distdl/nn/transpose.py
+++ src/distdl/nn/transpose.py
@@ -1,15 +1,67 @@
 import numpy as np
 import torch
+from mpi4py import MPI
 
 from distdl.utilities.slicing import compute_nd_slice_volume
 from distdl.utilities.slicing import compute_partition_intersection
+from distdl.utilities.slicing import compute_subsizes
 
 
 class DistributedTransposeFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input):
-        pass
+    def forward(ctx, input, parent_comm, sizes,
+                in_slices, in_buffers, in_comm,
+                out_slices, out_buffers, out_comm):
+
+        ctx.parent_comm = parent_comm
+        ctx.sizes = sizes
+
+        size = parent_comm.Get_size()
+
+        ctx.in_slices = in_slices
+        ctx.in_buffers = in_buffers
+        ctx.in_comm = in_comm
+
+        ctx.out_slices = out_slices
+        ctx.out_buffers = out_buffers
+        ctx.out_comm = out_comm
+
+        input_numpy = input.detach().numpy()
+
+        requests = []
+
+        # Recv my output parts
+        for r in range(size):
+            buff = out_buffers[r]
+            if buff is not None:
+                req = parent_comm.Irecv(buff, source=r, tag=0)
+                requests.append(req)
+
+        # Pack and send my input parts
+        for s in range(size):
+            buff = in_buffers[s]
+            if buff is not None:
+                sl = tuple(in_slices[s])
+                np.copyto(buff, input_numpy[sl].ravel())
+                req = parent_comm.Isend(buff, dest=s, tag=0)
+                requests.append(req)
+
+        MPI.Request.Waitall(requests)
+
+        coords = out_comm.Get_coords(out_comm.Get_rank())
+        out_sizes = compute_subsizes(out_comm.dims, coords, sizes)
+        output = np.zeros(out_sizes, dtype=input_numpy.dtype)
+
+        # Unpack my output parts
+        for r in range(size):
+            buff = out_buffers[r]
+            if buff is not None:
+                sl = tuple(out_slices[r])
+                sh = output[sl].shape
+                np.copyto(output[sl], buff.reshape(sh))
+
+        return torch.from_numpy(output)
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -18,11 +70,11 @@ class DistributedTransposeFunction(torch.autograd.Function):
 
 class DistributedTranspose(torch.nn.Module):
 
-    def __init__(self, sizes, base_comm, in_comm, out_comm):
+    def __init__(self, sizes, parent_comm, in_comm, out_comm):
         super(DistributedTranspose, self).__init__()
 
         self.sizes = sizes
-        self.base_comm = base_comm
+        self.parent_comm = parent_comm
         self.in_comm = in_comm
         self.out_comm = out_comm
 
