fa61022885eb6885a7ac1d96bb7fe3c72b779748
Russell J. Hewett <rhewett@vt.edu>
2020-05-12 12:56:43 -0400

    feat(#7) Moved slice intersection code to subroutine.

--- src/distdl/nn/transpose.py
+++ src/distdl/nn/transpose.py
@@ -1,10 +1,7 @@
-import numpy as np
 import torch
 
-from distdl.utilities.slicing import assemble_slices
-from distdl.utilities.slicing import compute_intersection
-from distdl.utilities.slicing import compute_starts
-from distdl.utilities.slicing import compute_stops
+from distdl.utilities.slicing import compute_nd_slice_volume
+from distdl.utilities.slicing import compute_partition_intersection
 
 
 class DistributedTransposeFunction(torch.autograd.Function):
@@ -28,74 +25,19 @@ class DistributedTranspose(torch.nn.Module):
         self.in_comm = in_comm
         self.out_comm = out_comm
 
-        in_dims = in_comm.dims
-        out_dims = out_comm.dims
+        in_slices = compute_partition_intersection(in_comm, out_comm, sizes)
+        in_buffer_sizes = [None if s is None else
+                           compute_nd_slice_volume(s) for s in in_slices]
 
-        # Prep forward send / adjoint recv phase
-        # Assumes input Cartesian communicator did not re-order ranks!
-        # Should be same as base_rank
-        in_rank = in_comm.Get_rank()
-        in_coords = in_comm.Get_coords(in_rank)
-        in_starts = compute_starts(in_dims, in_coords, sizes)
-        in_stops = compute_stops(in_dims, in_coords, sizes)
+        self.in_slices = in_slices
+        self.in_buffer_sizes = in_buffer_sizes
 
-        # Store slices of me
-        in_slices = []
-        # Store buffer sizes
-        in_buffer_sizes = []
+        out_slices = compute_partition_intersection(out_comm, in_comm, sizes)
+        out_buffer_sizes = [None if s is None else
+                            compute_nd_slice_volume(s) for s in out_slices]
 
-        # Build overlaps with output tensor
-        for p_out in range(out_comm.Get_size()):
-            p_out_coords = out_comm.Get_coords(p_out)
-            p_out_starts = compute_starts(out_dims, p_out_coords, sizes)
-            p_out_stops = compute_stops(out_dims, p_out_coords, sizes)
-
-            i_starts, i_stops, i_subsizes = compute_intersection(in_starts, in_stops,
-                                                                 p_out_starts, p_out_stops)
-
-            p_buffer_size = np.prod(i_subsizes)
-            if p_buffer_size == 0:
-                in_slices.append(None)
-                in_buffer_sizes.append(None)
-            else:
-                starts = i_starts - in_starts
-                stops = starts + i_subsizes
-                slices = assemble_slices(starts, stops)
-                in_slices.append(slices)
-                in_buffer_sizes.append(p_buffer_size)
-
-        # Prep forward recv / adjoint send phase
-        # Assumes input Cartesian communicator did not re-order ranks!
-        # Should be same as base_rank
-        out_rank = out_comm.Get_rank()
-        out_coords = out_comm.Get_coords(out_rank)
-        out_starts = compute_starts(out_dims, out_coords, sizes)
-        out_stops = compute_stops(out_dims, out_coords, sizes)
-
-        # Store slices of me
-        out_slices = []
-        # Store buffer sizes
-        out_buffer_sizes = []
-
-        # Build overlaps with output tensor
-        for p_in in range(in_comm.Get_size()):
-            p_in_coords = in_comm.Get_coords(p_in)
-            p_in_starts = compute_starts(in_dims, p_in_coords, sizes)
-            p_in_stops = compute_stops(in_dims, p_in_coords, sizes)
-
-            i_starts, i_stops, i_subsizes = compute_intersection(out_starts, out_stops,
-                                                                 p_in_starts, p_in_stops)
-
-            p_buffer_size = np.prod(i_subsizes)
-            if p_buffer_size == 0:
-                out_slices.append(None)
-                out_buffer_sizes.append(None)
-            else:
-                starts = i_starts - out_starts
-                stops = starts + i_subsizes
-                slices = assemble_slices(starts, stops)
-                out_slices.append(slices)
-                out_buffer_sizes.append(p_buffer_size)
+        self.out_slices = out_slices
+        self.out_buffer_sizes = out_buffer_sizes
 
     def forward(self, input):
 
--- src/distdl/utilities/slicing.py
+++ src/distdl/utilities/slicing.py
@@ -47,3 +47,55 @@ def assemble_slices(starts, stops):
         slices.append(slice(start, stop, None))
 
     return slices
+
+
+def compute_partition_intersection(partition_A_comm,
+                                   partition_B_comm,
+                                   x_sizes):
+
+    # Setup the outputs
+    x_r_slices = []
+
+    partition_A_dims = partition_A_comm.dims
+    partition_B_dims = partition_B_comm.dims
+
+    # Assumes Cartesian communicators did not re-order ranks!
+    x_r_rank = partition_A_comm.Get_rank()
+    x_r_coords = partition_A_comm.Get_coords(x_r_rank)
+
+    # Extract the subtensor description
+    x_r_starts = compute_starts(partition_A_dims, x_r_coords, x_sizes)
+    x_r_stops = compute_stops(partition_A_dims, x_r_coords, x_sizes)
+
+    # Build overlaps with full tensor by iterating over each worker
+    # and computing its subtensor of the full tensor, then figuring out
+    # any overlap.
+    for s in range(partition_B_comm.Get_size()):
+        x_s_coords = partition_B_comm.Get_coords(s)
+        x_s_starts = compute_starts(partition_B_dims, x_s_coords, x_sizes)
+        x_s_stops = compute_stops(partition_B_dims, x_s_coords, x_sizes)
+
+        x_i_starts, x_i_stops, x_i_subsizes = compute_intersection(x_r_starts,
+                                                                   x_r_stops,
+                                                                   x_s_starts,
+                                                                   x_s_stops)
+
+        x_i_volume = np.prod(x_i_subsizes)
+
+        # If the volume of the intersection is 0, we have no slice,
+        # otherwise we need to determine the slices for x_i relative to
+        # coordinates of x_r
+        if x_i_volume == 0:
+            x_r_slices.append(None)
+        else:
+            x_i_starts_rel_r = x_i_starts - x_r_starts
+            x_i_stops_rel_r = x_i_starts_rel_r + x_i_subsizes
+            x_i_slices_rel_r = assemble_slices(x_i_starts_rel_r, x_i_stops_rel_r)
+            x_r_slices.append(x_i_slices_rel_r)
+
+    return x_r_slices
+
+
+def compute_nd_slice_volume(slices):
+
+    return np.prod([s.stop-s.start for s in slices])
