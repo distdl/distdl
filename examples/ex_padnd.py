1bcc22de4c04269a9723d070d300c83695eace53
Thomas Grady <thomasg2@vt.edu>
2020-05-14 17:32:10 -0400

    feat(#9) Added N-D padding layer example

---
+++ examples/ex_padnd.py
@@ -0,0 +1,16 @@
+import torch
+
+import distdl.nn.padnd as padnd
+from distdl.utilities.misc import Bunch
+
+t = torch.ones(3, 4)
+print(f't =\n{t}')
+
+ctx = Bunch()
+pad_width = [(1, 2), (3, 4)]
+
+t_padded = padnd.PadNdFunction.forward(ctx, t, pad_width, value=0)
+print(f't_padded =\n{t_padded}')
+
+t_unpadded = padnd.PadNdFunction.backward(ctx, t_padded)[0]
+print(f't_unpadded =\n{t_unpadded}')
