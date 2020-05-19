8200009898e399127837c44a9b11a49fcda945f5
Russell J. Hewett <rhewett@vt.edu>
2020-05-19 10:57:54 -0400

    refactor(#27) Update transpose example reacting to #26.

--- examples/ex_tranpose.py
+++ examples/ex_tranpose.py
@@ -4,53 +4,64 @@ from mpi4py import MPI
 
 import distdl.nn.transpose as transpose
 import distdl.utilities.slicing as slicing
+from distdl.backends.mpi.partition import MPIPartition
 from distdl.utilities.debug import print_sequential
 from distdl.utilities.misc import Bunch
 
 # Set up MPI cartesian communicator
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+P_world = MPIPartition(MPI.COMM_WORLD)
+ranks = np.arange(P_world.size)
 
 in_dims = (4, 1)
-in_comm = comm.Create_cart(dims=in_dims)
-
-in_rank = in_comm.Get_rank()
-in_size = in_comm.Get_size()
-
-
 out_dims = (2, 2)
-out_comm = comm.Create_cart(dims=out_dims)
 
-out_rank = out_comm.Get_rank()
-out_size = out_comm.Get_size()
+in_size = np.prod(in_dims)
+out_size = np.prod(out_dims)
+
+# In partition is the first in_size ranks
+in_ranks = ranks[:in_size]
+P_in = P_world.create_subpartition(in_ranks)
+P_in_cart = P_in.create_cartesian_subpartition(in_dims)
 
 
-sizes = np.array([7, 5])
-layer = transpose.DistributedTranspose(sizes, comm, in_comm, out_comm)
+# Out partition is the last outsize ranks
+out_ranks = ranks[-out_size:]
+P_out = P_world.create_subpartition(out_ranks)
+P_out_cart = P_out.create_cartesian_subpartition(out_dims)
+
+tensor_sizes = np.array([7, 5])
+layer = transpose.DistributedTranspose(tensor_sizes, P_in_cart, P_out_cart)
 
 f = transpose.DistributedTransposeFunction
 
-in_subsizes = slicing.compute_subsizes(in_comm.dims,
-                                       in_comm.Get_coords(in_rank),
-                                       sizes)
-x = np.zeros(in_subsizes) + in_rank + 1
+if P_in_cart.active:
+    in_subsizes = slicing.compute_subsizes(P_in_cart.comm.dims,
+                                           P_in_cart.comm.Get_coords(P_in.rank),
+                                           tensor_sizes)
+    x = np.zeros(in_subsizes) + P_in.rank + 1
+    x = torch.from_numpy(x)
+else:
+    x = None
 
-x = torch.from_numpy(x)
-print_sequential(comm, f"x_{rank}: {x}")
+print_sequential(P_world.comm, f"x_{P_world.rank}: {x}")
 
 ctx = Bunch()
 
-y = f.forward(ctx, x, layer.parent_comm, layer.sizes,
-              layer.in_slices, layer.in_buffers, layer.in_comm,
-              layer.out_slices, layer.out_buffers, layer.out_comm)
-print_sequential(comm, f"y_{rank}: {y}")
+y = f.forward(ctx, x, layer.P_common, layer.sizes,
+              layer.P_in, layer.in_data, layer.in_buffers,
+              layer.P_out, layer.out_data, layer.out_buffers)
+print_sequential(P_world.comm, f"y_{P_world.rank}: {y}")
 
-out_subsizes = slicing.compute_subsizes(out_comm.dims,
-                                        out_comm.Get_coords(out_rank),
-                                        sizes)
-gy = np.zeros(out_subsizes) + out_rank + 1
-gy = torch.from_numpy(gy)
-print_sequential(comm, f"gy_{rank}: {gy}")
+if P_out_cart.active:
+    out_subsizes = slicing.compute_subsizes(P_out_cart.comm.dims,
+                                            P_out_cart.comm.Get_coords(P_out.rank),
+                                            tensor_sizes)
+    gy = np.zeros(out_subsizes) + P_out.rank + 1
+    gy = torch.from_numpy(gy)
+else:
+    gy = None
+
+print_sequential(P_world.comm, f"gy_{P_world.rank}: {gy}")
 
 gx = f.backward(ctx, gy)
-print_sequential(comm, f"gx_{rank}: {gx}")
+print_sequential(P_world.comm, f"gx_{P_world.rank}: {gx}")
