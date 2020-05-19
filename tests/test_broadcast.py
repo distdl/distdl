1f36748a1b8de3e2effe8b70220bf32cbcb91a0b
Russell J. Hewett <rhewett@vt.edu>
2020-05-19 12:40:54 -0400

    refactor(#28) Updated broadcast test to use partitions.
    This also brings this test in line with the current implementation of
    the transpose test.

--- tests/test_broadcast.py
+++ tests/test_broadcast.py
@@ -4,60 +4,85 @@ def test_broadcast_parallel():
     import torch
     from mpi4py import MPI
 
+    from distdl.backends.mpi.partition import MPIPartition
     from distdl.nn.broadcast import Broadcast
     from distdl.nn.broadcast import BroadcastFunction
     from distdl.utilities.misc import Bunch
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
+    P_world = MPIPartition(MPI.COMM_WORLD)
+    ranks = np.arange(P_world.size)
 
-    layer = Broadcast(comm=comm, root=0)
+    if P_world.size > 1:
+        # This should be a separate test...
+        use_ranks = ranks[1:]
+        P_use = P_world.create_subpartition(use_ranks)
+    else:
+        P_use = P_world
 
-    tensor_size = [7, 5]
+    tensor_sizes = np.array([7, 5])
+    root_rank = 0
+    layer = Broadcast(P_use, root=root_rank)
 
-    # Forward Input
-    x = torch.Tensor(np.random.randn(*tensor_size))
+    if P_use.active:
+        # Forward Input
+        x = torch.Tensor(np.random.randn(*tensor_sizes))
+        x_clone = x.clone()
 
-    # Adjoint Input
-    y = torch.Tensor(np.random.randn(*tensor_size))
+        # Adjoint Input
+        y = torch.Tensor(np.random.randn(*tensor_sizes))
+        y_clone = y.clone()
+    else:
+        x = None
+        x_clone = None
+        y = None
+        y_clone = None
 
     ctx = Bunch()
 
     # Apply A
-    Ax = BroadcastFunction.forward(ctx, x.clone(), layer.comm, layer.root)
+    Ax = BroadcastFunction.forward(ctx, x_clone, layer.partition, layer.root)
 
     # Apply A*
-    Asy = BroadcastFunction.backward(ctx, y.clone())[0]
+    Asy = BroadcastFunction.backward(ctx, y_clone)[0]
 
+    local_results = np.zeros(6, dtype=np.float64)
+    global_results = np.zeros(6, dtype=np.float64)
+
+    # Compute all of the local norms and inner products.
     # We only perform the inner product calculation between
-    # x and Asy on rank 0, as the input space of the forward
+    # x and Asy on the root rank, as the input space of the forward
     # operator and the output space of the adjoint operator
     # are only relevant to the root rank
-    if rank == 0:
-        norm_x = np.sqrt((torch.norm(x) ** 2).numpy())
-        norm_Asy = np.sqrt((torch.norm(x) ** 2).numpy())
-        ip2 = np.array([torch.sum(torch.mul(Asy, x))])
+    if P_use.rank == root_rank:
+        # ||x||^2
+        local_results[0] = (torch.norm(x)**2).numpy()
+        # ||A*@y||^2
+        local_results[3] = (torch.norm(Asy)**2).numpy()
+        # <A*@y, x>
+        local_results[5] = np.array([torch.sum(torch.mul(Asy, x))])
 
-    norm_y = (torch.norm(y)**2).numpy()
-    result = np.array([0.0], dtype=norm_y.dtype)
-    comm.Reduce(norm_y, result, op=MPI.SUM, root=0)
-    norm_y = np.sqrt(result)
+    if P_use.active:
+        # ||y||^2
+        local_results[1] = (torch.norm(y)**2).numpy()
+        # ||A@x||^2
+        local_results[2] = (torch.norm(Ax)**2).numpy()
+        # <A@x, y>
+        local_results[4] = np.array([torch.sum(torch.mul(Ax, y))])
 
-    norm_Ax = (torch.norm(Ax)**2).numpy()
-    result = np.array([0.0], dtype=norm_Ax.dtype)
-    comm.Reduce(norm_Ax, result, op=MPI.SUM, root=0)
-    norm_Ax = np.sqrt(result)
-
-    ip1 = np.array([torch.sum(torch.mul(y, Ax))])
-    result = np.array([0.0], dtype=ip1.dtype)
-    comm.Reduce(ip1, result, op=MPI.SUM, root=0)
-    ip1[:] = result[:]
+    # Reduce the norms and inner products
+    P_world.comm.Reduce(local_results, global_results, op=MPI.SUM, root=0)
 
     # Because this is being computed in parallel, we risk that these norms
     # and inner products are not exactly equal, because the floating point
     # arithmetic is not commutative.  The only way to fix this is to collect
     # the results to a single rank to do the test.
-    if(rank == 0):
+    if(P_world.rank == 0):
+        # Correct the norms from distributed calculation
+        global_results[:4] = np.sqrt(global_results[:4])
+
+        # Unpack the values
+        norm_x, norm_y, norm_Ax, norm_Asy, ip1, ip2 = global_results
+
         d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
         print(f"Adjoint test: {ip1/d} {ip2/d}")
         assert(np.isclose(ip1/d, ip2/d))
@@ -72,15 +97,13 @@ def test_broadcast_sequential():
     import torch
     from mpi4py import MPI
 
+    from distdl.backends.mpi.partition import MPIPartition
     from distdl.nn.broadcast import Broadcast
     from distdl.nn.broadcast import BroadcastFunction
     from distdl.utilities.misc import Bunch
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
     # Isolate a single processor to use for this test.
-    if rank == 0:
+    if MPI.COMM_WORLD.Get_rank() == 0:
         color = 0
         comm = MPI.COMM_WORLD.Split(color)
     else:
@@ -88,31 +111,33 @@ def test_broadcast_sequential():
         comm = MPI.COMM_WORLD.Split(color)
         return
 
-    layer = Broadcast(comm=comm, root=0)
+    P_world = MPIPartition(comm)
 
-    tensor_size = [7, 5]
+    layer = Broadcast(P_world)
+
+    tensor_sizes = np.array([7, 5])
 
     # Forward Input
-    x = torch.Tensor(np.random.randn(*tensor_size))
+    x = torch.Tensor(np.random.randn(*tensor_sizes))
 
     # Adjoint Input
-    y = torch.Tensor(np.random.randn(*tensor_size))
+    y = torch.Tensor(np.random.randn(*tensor_sizes))
 
     ctx = Bunch()
 
     # Apply A
-    Ax = BroadcastFunction.forward(ctx, x.clone(), layer.comm, layer.root)
+    Ax = BroadcastFunction.forward(ctx, x.clone(), layer.partition, layer.root)
 
     # Apply A*
     Asy = BroadcastFunction.backward(ctx, y.clone())[0]
 
-    norm_x = np.sqrt((torch.norm(x) ** 2).numpy())
-    norm_Asy = np.sqrt((torch.norm(x) ** 2).numpy())
-    ip2 = np.array([torch.sum(torch.mul(Asy, x))])
-
+    norm_x = np.sqrt((torch.norm(x)**2).numpy())
     norm_y = np.sqrt((torch.norm(y)**2).numpy())
     norm_Ax = np.sqrt((torch.norm(Ax)**2).numpy())
+    norm_Asy = np.sqrt((torch.norm(Asy)**2).numpy())
+
     ip1 = np.array([torch.sum(torch.mul(y, Ax))])
+    ip2 = np.array([torch.sum(torch.mul(Asy, x))])
 
     d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
     print(f"Adjoint test: {ip1/d} {ip2/d}")
