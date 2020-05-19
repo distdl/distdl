28fbee2b6d0b81edfbea651f38d68e69d768e761
Thomas Grady <thomasg2@vt.edu>
2020-05-19 16:35:37 -0400

    Made bunch dict subclass torch context mixin to allow for proper function tests

--- src/distdl/utilities/misc.py
+++ src/distdl/utilities/misc.py
@@ -1,9 +1,15 @@
-class Bunch(dict):
+from torch.autograd.function import _ContextMethodMixin
+
+
+class Bunch(dict, _ContextMethodMixin):
     def __getattribute__(self, key):
         try:
             return self[key]
         except KeyError:
-            raise AttributeError(key)
+            try:
+                return object.__getattribute__(self, key)
+            except AttributeError:
+                raise AttributeError(key)
 
     def __setattr__(self, key, value):
         self[key] = value
