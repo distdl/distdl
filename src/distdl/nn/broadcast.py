570a7d6eaa5126c0540ec7c0f893f342911ecd78
Russell J. Hewett <rhewett@vt.edu>
2020-05-19 12:40:08 -0400

    refactor(#28) Updated broadcast layer to use partitions.
    This is in reaction to #26.

--- src/distdl/nn/broadcast.py
+++ src/distdl/nn/broadcast.py
@@ -6,48 +6,54 @@ from mpi4py import MPI
 class BroadcastFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input, comm, root):
+    def forward(ctx, input, partition, root):
 
-        size = comm.Get_size()
-
-        ctx.comm = comm
-        ctx.size = size
+        ctx.partition = partition
         ctx.root = root
 
-        if size == 1:
+        if partition.size == 1:
             return input.clone()
 
-        input_numpy = input.detach().numpy()
+        if partition.active:
+            input_numpy = input.detach().numpy()
 
-        comm.Bcast(input_numpy, root=root)
+            partition.comm.Bcast(input_numpy, root=root)
 
-        return torch.tensor(input_numpy, requires_grad=input.requires_grad)
+            return torch.tensor(input_numpy,
+                                requires_grad=input.requires_grad)
+        else:
+            return None
 
     @staticmethod
     def backward(ctx, grad_output):
 
-        comm = ctx.comm
-        size = ctx.size
+        partition = ctx.partition
         root = ctx.root
 
-        if size == 1:
+        if partition.size == 1:
             return grad_output.clone(), None, None
 
-        grad_output_numpy = grad_output.detach().numpy()
-        reduced_data = np.zeros(shape=grad_output_numpy.shape, dtype=grad_output_numpy.dtype)
+        if partition.active:
+            grad_output_numpy = grad_output.detach().numpy()
+            reduced_data = np.zeros(shape=grad_output_numpy.shape,
+                                    dtype=grad_output_numpy.dtype)
 
-        comm.Reduce(grad_output_numpy, reduced_data, root=root, op=MPI.SUM)
+            partition.comm.Reduce(grad_output_numpy, reduced_data,
+                                  root=root, op=MPI.SUM)
 
-        return torch.tensor(reduced_data, requires_grad=grad_output.requires_grad), None, None
+            return torch.tensor(reduced_data,
+                                requires_grad=grad_output.requires_grad), None, None
+        else:
+            return None, None, None
 
 
 class Broadcast(torch.nn.Module):
 
-    def __init__(self, comm, root=0):
+    def __init__(self, partition, root=0):
         super(Broadcast, self).__init__()
 
-        self.comm = comm
+        self.partition = partition
         self.root = root
 
     def forward(self, input):
-        return BroadcastFunction.apply(input, self.comm, self.root)
+        return BroadcastFunction.apply(input, self.partition, self.root)
