7f690967cf01cbfd2ff1caa275984a364562de25
Thomas Grady <thomasg2@vt.edu>
2020-05-25 17:45:37 -0400

    bug(#60) Fix PadNd to work from non-active ranks

--- src/distdl/nn/padnd.py
+++ src/distdl/nn/padnd.py
@@ -1,13 +1,19 @@
 import numpy as np
 import torch
 
+from distdl.utilities.torch import NoneTensor
+
 
 class PadNdFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input, pad_width, value):
+    def forward(ctx, input, pad_width, value, partition):
 
         ctx.pad_width = pad_width
+        ctx.partition = partition
+
+        if not partition.active:
+            return NoneTensor()
 
         input_numpy = input.detach().numpy()
 
@@ -19,6 +25,10 @@ class PadNdFunction(torch.autograd.Function):
     def backward(ctx, grad_output):
 
         pad_width = ctx.pad_width
+        partition = ctx.partition
+
+        if not partition.active:
+            return NoneTensor(), None, None, None
 
         slices = []
         for (lpad, rpad) in pad_width:
@@ -30,17 +40,18 @@ class PadNdFunction(torch.autograd.Function):
 
         result = grad_output_numpy[tuple(slices)]
 
-        return torch.tensor(result, requires_grad=grad_output.requires_grad), None, None
+        return torch.tensor(result, requires_grad=grad_output.requires_grad), None, None, None
 
 
 class PadNd(torch.nn.Module):
 
-    def __init__(self, pad_width, value=0):
+    def __init__(self, pad_width, value, partition):
 
         super(PadNd, self).__init__()
 
         self.pad_width = pad_width
         self.value = value
+        self.partition = partition
 
     def forward(self, input):
-        return PadNdFunction.apply(input, self.pad_width, self.value)
+        return PadNdFunction.apply(input, self.pad_width, self.value, self.partition)
