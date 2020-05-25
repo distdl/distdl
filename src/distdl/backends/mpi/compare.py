443d7dc76548a27c853aa51bbdd60a625a91e9d5
Russell J. Hewett <rhewett@vt.edu>
2020-05-25 12:13:58 -0400

    refactor(#57) Made group and comm comparison safe.

--- src/distdl/backends/mpi/compare.py
+++ src/distdl/backends/mpi/compare.py
@@ -2,10 +2,14 @@ from mpi4py import MPI
 
 
 def check_identical_comm(c1, c2):
+    if check_null_comm(c1) or check_null_comm(c2):
+        return False
     return MPI.Comm.Compare(c1, c2) == MPI.IDENT
 
 
 def check_identical_group(g1, g2):
+    if check_null_group(g1) or check_null_group(g2):
+        return False
     return MPI.Group.Compare(g1, g2) == MPI.IDENT
 
 
