2cba452e22bbea5833e59fdcc482da17d8e4d2e9
Thomas Grady <thomasg2@vt.edu>
2020-05-26 15:52:41 -0400

    feat(#11) Added 2d average pooling layer example

---
+++ examples/ex_average_pooling_2d.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+from mpi4py import MPI
+
+from distdl.backends.mpi.partition import MPIPartition
+from distdl.nn.pooling import DistributedAvgPool2d
+from distdl.utilities.debug import print_sequential
+from distdl.utilities.slicing import compute_subsizes
+from distdl.utilities.torch import NoneTensor
+
+P_world = MPIPartition(MPI.COMM_WORLD)
+P_world.comm.Barrier()
+
+P = P_world.create_partition_inclusive(np.arange(4))
+P_cart = P.create_cartesian_topology_partition([1, 1, 2, 2])
+
+global_tensor_sizes = np.array([1, 1, 11, 10])
+
+layer = DistributedAvgPool2d(global_tensor_sizes, P_cart, kernel_size=[2, 2])
+
+x = NoneTensor()
+if P_cart.active:
+    input_tensor_sizes = compute_subsizes(P_cart.dims, P_cart.cartesian_coordinates(P_cart.rank), global_tensor_sizes)
+    x = torch.tensor(np.ones(shape=input_tensor_sizes) * (P_cart.rank + 1), dtype=float)
+x.requires_grad = True
+
+print_sequential(P_world.comm, f'rank = {P_world.rank}, input =\n{x}')
+
+y = layer(x)
+
+print_sequential(P_world.comm, f'rank = {P_world.rank}, output =\n{y}')
