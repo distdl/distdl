28a5b665a6477976a72aeed3a3171528d461a00e
Russell J. Hewett <rhewett@vt.edu>
2020-05-26 22:48:20 -0400

    refactor(#62) Updated transpose layer to use partition from #57.

--- src/distdl/nn/transpose.py
+++ src/distdl/nn/transpose.py
@@ -2,20 +2,22 @@ import numpy as np
 import torch
 from mpi4py import MPI
 
+from distdl.backends.mpi.partition import MPIPartition
 from distdl.utilities.slicing import compute_nd_slice_volume
 from distdl.utilities.slicing import compute_partition_intersection
 from distdl.utilities.slicing import compute_subsizes
 from distdl.utilities.slicing import range_coords
+from distdl.utilities.torch import NoneTensor
 
 
 class DistributedTransposeFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input, P_common, sizes,
+    def forward(ctx, input, P_union, sizes,
                 P_in, in_data, in_buffers,
-                P_out, out_data, out_buffers):
+                P_out, out_data, out_buffers, dtype):
 
-        ctx.P_common = P_common
+        ctx.P_union = P_union
         ctx.sizes = sizes
 
         ctx.P_in = P_in
@@ -26,17 +28,33 @@ class DistributedTransposeFunction(torch.autograd.Function):
         ctx.out_data = out_data
         ctx.out_buffers = out_buffers
 
+        ctx.dtype = dtype
+
+        input_requires_grad = False
+        # By design, P_in is always first in the union
+        if P_union.active:
+            if P_in.rank == 0:
+                input_requires_grad = input.requires_grad
+                P_union.comm.Bcast(np.array([1 if input_requires_grad else 0]),
+                                   root=0)
+            else:
+                irg = np.array([0], dtype=np.int)
+                P_union.comm.Bcast(irg, root=0)
+                input_requires_grad = bool(irg[0] == 1)
+
+        ctx.input_requires_grad = input_requires_grad
+
         requests = []
 
         # Default everyone to output nothing
-        output = None
+        output = NoneTensor()
 
         # If I am getting data, recv my output parts
         recv_count = 0
         if P_out.active:
             for (sl, sz, partner), buff in zip(out_data, out_buffers):
                 if buff is not None:
-                    req = P_common.comm.Irecv(buff, source=partner, tag=111)
+                    req = P_union.comm.Irecv(buff, source=partner, tag=111)
                     requests.append(req)
                 else:
                     # We add this if there is no recv so that the indices of
@@ -52,7 +70,7 @@ class DistributedTransposeFunction(torch.autograd.Function):
             for (sl, sz, partner), buff in zip(in_data, in_buffers):
                 if buff is not None:
                     np.copyto(buff, input_numpy[tuple(sl)].ravel())
-                    req = P_common.comm.Isend(buff, dest=partner, tag=111)
+                    req = P_union.comm.Isend(buff, dest=partner, tag=111)
                     requests.append(req)
                 else:
                     # We add this for symmetry, but don't really need it.
@@ -66,7 +84,7 @@ class DistributedTransposeFunction(torch.autograd.Function):
             out_sizes = compute_subsizes(P_out.comm.dims, coords, sizes)
             # TODO(#25): The dtype should not be fixed, but correcting this is
             #            a thing that needs to be resolved globally.
-            output = np.zeros(out_sizes, dtype=np.float64)
+            output = np.zeros(out_sizes, dtype=dtype)
 
         # Unpack the received data as it arrives
         completed_count = 0
@@ -88,13 +106,14 @@ class DistributedTransposeFunction(torch.autograd.Function):
 
         if P_out.active:
             output = torch.from_numpy(output)
+            output.requires_grad = input_requires_grad
 
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
 
-        P_common = ctx.P_common
+        P_union = ctx.P_union
         sizes = ctx.sizes
 
         P_in = ctx.P_in
@@ -105,17 +124,21 @@ class DistributedTransposeFunction(torch.autograd.Function):
         out_data = ctx.out_data
         out_buffers = ctx.out_buffers
 
+        dtype = ctx.dtype
+
+        input_requires_grad = ctx.input_requires_grad
+
         requests = []
 
         # Default everyone to output None
-        grad_input = None
+        grad_input = NoneTensor()
 
         # Recv my input parts
         recv_count = 0
         if P_in.active:
             for (sl, sz, partner), buff in zip(in_data, in_buffers):
                 if buff is not None:
-                    req = P_common.comm.Irecv(buff, source=partner, tag=113)
+                    req = P_union.comm.Irecv(buff, source=partner, tag=113)
                     requests.append(req)
                 else:
                     # We add this if there is no recv so that the indices of
@@ -131,7 +154,7 @@ class DistributedTransposeFunction(torch.autograd.Function):
             for (sl, sz, partner), buff in zip(out_data, out_buffers):
                 if buff is not None:
                     np.copyto(buff, grad_output_numpy[tuple(sl)].ravel())
-                    req = P_common.comm.Isend(buff, dest=partner, tag=113)
+                    req = P_union.comm.Isend(buff, dest=partner, tag=113)
                     requests.append(req)
                 else:
                     # We add this for symmetry, but don't really need it.
@@ -143,7 +166,7 @@ class DistributedTransposeFunction(torch.autograd.Function):
             in_sizes = compute_subsizes(P_in.comm.dims, coords, sizes)
             # TODO(#25): The dtype should not be fixed, but correcting this is
             #            a thing that needs to be resolved globally.
-            grad_input = np.zeros(in_sizes, dtype=np.float64)
+            grad_input = np.zeros(in_sizes, dtype=dtype)
 
         # Unpack the received data as it arrives
         completed_count = 0
@@ -167,8 +190,9 @@ class DistributedTransposeFunction(torch.autograd.Function):
 
         if P_in.active:
             grad_input = torch.from_numpy(grad_input)
+            grad_input.requires_grad = input_requires_grad
 
-        return grad_input, None, None, None, None, None, None, None, None
+        return grad_input, None, None, None, None, None, None, None, None, None
 
 
 class DistributedTranspose(torch.nn.Module):
@@ -180,23 +204,84 @@ class DistributedTranspose(torch.nn.Module):
         self.P_in = P_in
         self.P_out = P_out
 
-        # Find the common partition between the input and output partitions
-        P_common = P_in.common_ancestor(P_out)
-        if P_common is None:
-            raise Exception()
-        self.P_common = P_common
-
         self.in_data = []
         self.out_data = []
 
-        in_dims = P_in.dims
-        out_dims = P_out.dims
+        self.in_buffers = None
+        self.out_buffers = None
 
-        # We need to move data between two potentially disjoint partitions,
-        # so we have to find a common mapping, which requires the common
-        # partition.
-        P_common_to_P_in = P_in.map_from_ancestor(P_common)
-        P_common_to_P_out = P_out.map_from_ancestor(P_common)
+        # TODO(#25): The dtype should not be fixed, but correcting this is
+        #            a thing that needs to be resolved globally.
+        self.dtype = np.float32
+
+        self.identity = False
+
+        if P_in == P_out:
+            self.identity = True
+            return
+
+        P_union = MPIPartition(MPI.COMM_NULL)
+        if P_in.active or P_out.active:
+            P_union = P_in.create_partition_union(P_out)
+        self.P_union = P_union
+
+        if not P_union.active:
+            # This is where the early exit stuff will go
+            return
+
+        # Find the rank in P_union with rank 0 of P_in
+        rank_map_data = np.array([-1], dtype=np.int)
+        if P_in.active:
+            rank_map_data[0] = P_in.rank
+        rank_map = -1*np.ones(P_union.size, dtype=np.int)
+        P_union.comm.Allgather(rank_map_data, rank_map)
+        in_root = np.where(rank_map == 0)[0][0]
+
+        # Find the rank in P_union with rank 0 of P_dest
+        rank_map_data = np.array([-1], dtype=np.int)
+        if P_out.active:
+            rank_map_data[0] = P_out.rank
+        rank_map = -1*np.ones(P_union.size, dtype=np.int)
+        P_union.comm.Allgather(rank_map_data, rank_map)
+        out_root = np.where(rank_map == 0)[0][0]
+
+        # Share the in cartesian dimension with everyone
+        in_dim = np.zeros(1, dtype=np.int)
+        if P_in.active and P_in.rank == 0:
+            in_dim[0] = P_in.dim
+        P_union.comm.Bcast(in_dim, root=in_root)
+
+        # Share the out cartesian dimension with everyone
+        out_dim = np.zeros(1, dtype=np.int)
+        if P_out.active and P_out.rank == 0:
+            out_dim[0] = P_out.dim
+        P_union.comm.Bcast(out_dim, root=out_root)
+
+        in_dims = np.ones(in_dim, dtype=np.int)
+        if P_in.active and P_in.rank == 0:
+            in_dims = P_in.dims
+        P_union.comm.Bcast(in_dims, root=in_root)
+
+        # Share the out partition dimensions with everyone
+        out_dims = np.zeros(out_dim, dtype=np.int)
+        if P_out.active and P_out.rank == 0:
+            out_dims = P_out.dims
+        P_union.comm.Bcast(out_dims, root=out_root)
+
+        in_index = -1
+        if P_in.active:
+            in_index = P_in.rank
+        out_index = -1
+        if P_out.active:
+            out_index = P_out.rank
+
+        # Share the two indices with every worker in the union.  The first
+        # column of data contains the source index and the second contains
+        # the destination index.
+        union_indices = -1*np.ones(2*self.P_union.size, dtype=np.int)
+        local_indices = np.array([in_index, out_index], dtype=np.int)
+        self.P_union.comm.Allgather(local_indices, union_indices)
+        union_indices.shape = (-1, 2)
 
         # We only need to move data to the output partition if we actually
         # have input data.  It is possible to have both input and output data,
@@ -205,7 +290,7 @@ class DistributedTranspose(torch.nn.Module):
             in_coords = P_in.cartesian_coordinates(P_in.rank)
 
             # Compute our overlaps for each output subpartition.
-            for rank, out_coords in enumerate(range_coords(P_out.dims)):
+            for rank, out_coords in enumerate(range_coords(out_dims)):
                 sl = compute_partition_intersection(in_dims, in_coords,
                                                     out_dims, out_coords,
                                                     sizes)
@@ -213,10 +298,10 @@ class DistributedTranspose(torch.nn.Module):
                     sz = compute_nd_slice_volume(sl)
                     # Reverse the mapping to get the output partner's rank in
                     # the common partition.
-                    partner = np.where(P_common_to_P_out == rank)[0][0]
+                    partner = np.where(union_indices[:, 1] == rank)[0][0]
                     self.in_data.append((sl, sz, partner))
                 else:
-                    self.in_data.append((None, None, P_out.null_rank))
+                    self.in_data.append((None, None, None))
 
         # We only need to obtain data from the input partition if we actually
         # have output data.
@@ -224,7 +309,7 @@ class DistributedTranspose(torch.nn.Module):
             out_coords = P_out.cartesian_coordinates(P_out.rank)
 
             # Compute our overlaps for each input subpartition.
-            for rank, in_coords in enumerate(range_coords(P_in.dims)):
+            for rank, in_coords in enumerate(range_coords(in_dims)):
                 sl = compute_partition_intersection(out_dims, out_coords,
                                                     in_dims, in_coords,
                                                     sizes)
@@ -232,23 +317,14 @@ class DistributedTranspose(torch.nn.Module):
                     sz = compute_nd_slice_volume(sl)
                     # Reverse the mapping to get the input partner's rank in
                     # the common partition.
-                    partner = np.where(P_common_to_P_in == rank)[0][0]
+                    partner = np.where(union_indices[:, 0] == rank)[0][0]
                     self.out_data.append((sl, sz, partner))
                 else:
-                    self.out_data.append((None, None, P_in.null_rank))
+                    self.out_data.append((None, None, None))
 
-        # In the sequential case, don't allocate anything, we don't use them.
-        if P_in == P_out:
-            self.in_buffers = None
-            self.out_buffers = None
-            self.identity = True
-        else:
-            # TODO(#25): The dtype should not be fixed, but correcting this is
-            #            a thing that needs to be resolved globally.
-            buffs = self._allocate_buffers(np.float64)
-            self.in_buffers = buffs[0]
-            self.out_buffers = buffs[1]
-            self.identity = False
+        buffs = self._allocate_buffers(self.dtype)
+        self.in_buffers = buffs[0]
+        self.out_buffers = buffs[1]
 
     def _allocate_buffers(self, dtype):
 
@@ -273,10 +349,15 @@ class DistributedTranspose(torch.nn.Module):
     def forward(self, input):
 
         if self.identity:
-            if input is None:
-                return None
             return input.clone()
 
-        return DistributedTransposeFunction.apply(input, self.P_common, self.sizes,
-                                                  self.P_in, self.in_data, self.in_buffers,
-                                                  self.P_out, self.out_data, self.out_buffers)
+        return DistributedTransposeFunction.apply(input,
+                                                  self.P_union,
+                                                  self.sizes,
+                                                  self.P_in,
+                                                  self.in_data,
+                                                  self.in_buffers,
+                                                  self.P_out,
+                                                  self.out_data,
+                                                  self.out_buffers,
+                                                  self.dtype)
