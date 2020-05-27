9c9453bc15d0345e0ee7787194fe403d21d00284
Russell J. Hewett <rhewett@vt.edu>
2020-05-26 21:00:42 -0400

    feat(#56) Added fortan-index conversion utility

--- src/distdl/utilities/index_tricks.py
+++ src/distdl/utilities/index_tricks.py
@@ -1,5 +1,12 @@
-def cartesian_index(dims, coords):
+def cartesian_index_c(dims, coords):
     idx = 0
     for d, c in zip(dims, coords):
+        idx = idx*d + c
+    return idx
+
+
+def cartesian_index_f(dims, coords):
+    idx = 0
+    for d, c in zip(reversed(dims), reversed(coords)):
         idx = idx*d + c
     return idx
