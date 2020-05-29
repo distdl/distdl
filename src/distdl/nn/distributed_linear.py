aaced1c0132bea72e1e3dd92db0224700a00cb07
Russell J. Hewett <rhewett@vt.edu>
2020-05-29 10:30:47 -0400

    bug(#77) Fixed DistributedLinear so it takes global size as argument.

--- src/distdl/nn/distributed_linear.py
+++ src/distdl/nn/distributed_linear.py
@@ -2,6 +2,7 @@ import torch
 
 from distdl.nn.broadcast import Broadcast
 from distdl.nn.sum_reduce import SumReduce
+from distdl.utilities.slicing import compute_subsizes
 
 
 class DistributedLinear(torch.nn.Module):
@@ -21,13 +22,22 @@ class DistributedLinear(torch.nn.Module):
 
         self.x_broadcast = Broadcast(self.P_x, self.P_mul)
         if self.P_mul.active:
+            # x_subsizes is junk in the y dimension and vice versa
+            x_subsizes = compute_subsizes(P_mul.dims, P_mul.coords, x_sizes)
+            # Because of the way subsizes works, and because the y dimesion is
+            # transposed for the multiplication, we need to reverse the
+            # dimensions so that the structure of y matches.
+            y_subsizes = compute_subsizes(P_mul.dims[::-1], P_mul.coords[::-1], y_sizes)
             # On column 0, use the specified bias, otherwise no bias to
             # prevent double counting
             bias = self.bias if (self.P_mul.coords[-1] == 0) else False
-            self.sublinear = torch.nn.Linear(x_sizes[-1], y_sizes[-1], bias=bias)
+            self.sublinear = torch.nn.Linear(x_subsizes[-1], y_subsizes[-1], bias=bias)
         self.y_sum_reduce = SumReduce(self.P_mul, self.P_y, transpose_src=True)
 
     def forward(self, input):
+
+        if not (self.P_x.active or self.P_y.active or self.P_mul.active):
+            return input.clone()
 
         # broadcast x down the columns
         x = self.x_broadcast(input)
