a51449bc205e425c62a0d5050ebd7e77f94a7a14
Russell J. Hewett <rhewett@vt.edu>
2020-06-05 14:14:54 -0400

    refactor(#19) Added fixture for creating minimum required communicator.

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,21 @@ def barrier_fence_fixture():
     yield
 
     MPI.COMM_WORLD.Barrier()
+
+
+@pytest.mark.mpi
+@pytest.fixture(scope="function")
+def comm_split_fixture(request):
+    from mpi4py import MPI
+
+    min_size = request.param
+
+    # Isolate the required number of processors
+    if MPI.COMM_WORLD.rank < min_size:
+        color = 0
+        base_comm = MPI.COMM_WORLD.Split(color)
+        return base_comm, True
+    else:
+        color = 1
+        base_comm = MPI.COMM_WORLD.Split(color)
+        return base_comm, False
