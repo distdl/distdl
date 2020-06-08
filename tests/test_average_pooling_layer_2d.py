71df85f33e4723fb2eaea728ccd9fd1649e2c216
Russell J. Hewett <rhewett@vt.edu>
2020-06-08 16:22:23 -0400

    refactor(#19) Fixed pooling tests to use fixtures and parametrization.

--- tests/test_average_pooling_layer_2d.py
+++ tests/test_average_pooling_layer_2d.py
@@ -1,268 +1,95 @@
-# Weird case #1
-def test_average_pooling_layer_2d_1():
+import numpy as np
+import pytest
+from adjoint_test import check_adjoint_test_tight
+
+adjoint_parametrizations = []
+
+adjoint_parametrizations.append(
+    pytest.param(
+        np.arange(0, 4), [1, 1, 2, 2],  # P_x_ranks, P_x_topo
+        [1, 1, 7, 9],  # global_tensor_size
+        4,  # passed to comm_split_fixture, required MPI ranks
+        id="distributed-weird_1",
+        marks=[pytest.mark.mpi(min_size=4)]
+        )
+    )
+
+adjoint_parametrizations.append(
+    pytest.param(
+        np.arange(0, 9), [1, 1, 3, 3],  # P_x_ranks, P_x_topo
+        [1, 1, 18, 23],  # global_tensor_size
+        9,  # passed to comm_split_fixture, required MPI ranks
+        id="distributed-weird_2",
+        marks=[pytest.mark.mpi(min_size=9)]
+        )
+    )
+
+adjoint_parametrizations.append(
+    pytest.param(
+        np.arange(0, 4), [1, 1, 2, 2],  # P_x_ranks, P_x_topo
+        [1, 1, 8, 8],  # global_tensor_size
+        4,  # passed to comm_split_fixture, required MPI ranks
+        id="distributed-no_comm",
+        marks=[pytest.mark.mpi(min_size=4)]
+        )
+    )
+
+
+# For example of indirect, see https://stackoverflow.com/a/28570677
+@pytest.mark.parametrize("P_x_ranks, P_x_topo,"
+                         "global_tensor_size,"
+                         "comm_split_fixture",
+                         adjoint_parametrizations,
+                         indirect=["comm_split_fixture"])
+def test_average_pooling_adjoint_input(barrier_fence_fixture,
+                                       comm_split_fixture,
+                                       P_x_ranks, P_x_topo,
+                                       global_tensor_size):
 
     import numpy as np
     import torch
-    from mpi4py import MPI
 
     from distdl.backends.mpi.partition import MPIPartition
     from distdl.nn.pooling import DistributedAvgPool2d
     from distdl.utilities.slicing import compute_subsizes
     from distdl.utilities.torch import NoneTensor
 
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
+    # Isolate the minimum needed ranks
+    base_comm, active = comm_split_fixture
+    if not active:
+        return
+    P_world = MPIPartition(base_comm)
 
-    P = P_world.create_partition_inclusive(np.arange(4))
-    P_cart = P.create_cartesian_topology_partition([1, 1, 2, 2])
+    # Create the partitions
+    P_x_base = P_world.create_partition_inclusive(P_x_ranks)
+    P_x = P_x_base.create_cartesian_topology_partition(P_x_topo)
 
-    global_tensor_sizes = np.array([1, 1, 7, 9])
+    global_tensor_sizes = np.asarray(global_tensor_size)
 
-    layer = DistributedAvgPool2d(global_tensor_sizes, P_cart, kernel_size=[2, 2])
+    layer = DistributedAvgPool2d(global_tensor_sizes, P_x,
+                                 kernel_size=[2, 2],
+                                 stride=[2, 2])
 
     x = NoneTensor()
-    if P_cart.active:
-        input_tensor_sizes = compute_subsizes(P_cart.dims, P_cart.cartesian_coordinates(P_cart.rank), global_tensor_sizes)
+    if P_x.active:
+        input_tensor_sizes = compute_subsizes(P_x.dims,
+                                              P_x.coords,
+                                              global_tensor_sizes)
         x = torch.tensor(np.random.randn(*input_tensor_sizes))
     x.requires_grad = True
 
-    Ax = layer(x)
+    y = layer(x)
 
-    y = NoneTensor()
-    if P_cart.active:
-        y = torch.tensor(np.random.randn(*Ax.shape))
-    y.requires_grad = True
+    dy = NoneTensor()
+    if P_x.active:
+        dy = torch.Tensor(np.random.randn(*y.shape))
 
-    Ax.backward(y)
-    Asy = x.grad
-
-    x = x.detach()
-    Ax = Ax.detach()
-    y = y.detach()
-    Asy = Asy.detach()
-
-    local_results = np.zeros(6, dtype=np.float64)
-    global_results = np.zeros(6, dtype=np.float64)
-
-    # Compute all of the local norms and inner products.
-    # We only perform the inner product calculation between
-    # x and Asy on the root rank, as the input space of the forward
-    # operator and the output space of the adjoint operator
-    # are only relevant to the root rank
-    if P_cart.active:
-        # ||x||^2
-        local_results[0] = (torch.norm(x)**2).numpy()
-        # ||y||^2
-        local_results[1] = (torch.norm(y)**2).numpy()
-        # ||A@x||^2
-        local_results[2] = (torch.norm(Ax)**2).numpy()
-        # ||A*@y||^2
-        local_results[3] = (torch.norm(Asy)**2).numpy()
-        # <A@x, y>
-        local_results[4] = np.array([torch.sum(torch.mul(Ax, y))])
-        # <A*@y, x>
-        local_results[5] = np.array([torch.sum(torch.mul(Asy, x))])
-
-    # Reduce the norms and inner products
-    P_world.comm.Reduce(local_results, global_results, op=MPI.SUM, root=0)
-
-    # Because this is being computed in parallel, we risk that these norms
-    # and inner products are not exactly equal, because the floating point
-    # arithmetic is not commutative.  The only way to fix this is to collect
-    # the results to a single rank to do the test.
-    if(P_world.rank == 0):
-        # Correct the norms from distributed calculation
-        global_results[:4] = np.sqrt(global_results[:4])
-
-        # Unpack the values
-        norm_x, norm_y, norm_Ax, norm_Asy, ip1, ip2 = global_results
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-    else:
-        # All other ranks pass the adjoint test
-        assert(True)
-
-    # Barrier fence to ensure all enclosed MPI calls resolve.
-    P_world.comm.Barrier()
-
-
-# Weird case #2
-def test_average_pooling_layer_2d_2():
-
-    import numpy as np
-    import torch
-    from mpi4py import MPI
-
-    from distdl.backends.mpi.partition import MPIPartition
-    from distdl.nn.pooling import DistributedAvgPool2d
-    from distdl.utilities.slicing import compute_subsizes
-    from distdl.utilities.torch import NoneTensor
-
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
-
-    P = P_world.create_partition_inclusive(np.arange(9))
-    P_cart = P.create_cartesian_topology_partition([1, 1, 3, 3])
-
-    global_tensor_sizes = np.array([1, 1, 18, 23])
-
-    layer = DistributedAvgPool2d(global_tensor_sizes, P_cart, kernel_size=[4, 4])
-
-    x = NoneTensor()
-    if P_cart.active:
-        input_tensor_sizes = compute_subsizes(P_cart.dims, P_cart.cartesian_coordinates(P_cart.rank), global_tensor_sizes)
-        x = torch.tensor(np.random.randn(*input_tensor_sizes))
-    x.requires_grad = True
-
-    Ax = layer(x)
-
-    y = NoneTensor()
-    if P_cart.active:
-        y = torch.tensor(np.random.randn(*Ax.shape))
-    y.requires_grad = True
-
-    Ax.backward(y)
-    Asy = x.grad
+    y.backward(dy)
+    dx = x.grad
 
     x = x.detach()
-    Ax = Ax.detach()
+    dx = dx.detach()
+    dy = dy.detach()
     y = y.detach()
-    Asy = Asy.detach()
 
-    local_results = np.zeros(6, dtype=np.float64)
-    global_results = np.zeros(6, dtype=np.float64)
-
-    # Compute all of the local norms and inner products.
-    # We only perform the inner product calculation between
-    # x and Asy on the root rank, as the input space of the forward
-    # operator and the output space of the adjoint operator
-    # are only relevant to the root rank
-    if P_cart.active:
-        # ||x||^2
-        local_results[0] = (torch.norm(x)**2).numpy()
-        # ||y||^2
-        local_results[1] = (torch.norm(y)**2).numpy()
-        # ||A@x||^2
-        local_results[2] = (torch.norm(Ax)**2).numpy()
-        # ||A*@y||^2
-        local_results[3] = (torch.norm(Asy)**2).numpy()
-        # <A@x, y>
-        local_results[4] = np.array([torch.sum(torch.mul(Ax, y))])
-        # <A*@y, x>
-        local_results[5] = np.array([torch.sum(torch.mul(Asy, x))])
-
-    # Reduce the norms and inner products
-    P_world.comm.Reduce(local_results, global_results, op=MPI.SUM, root=0)
-
-    # Because this is being computed in parallel, we risk that these norms
-    # and inner products are not exactly equal, because the floating point
-    # arithmetic is not commutative.  The only way to fix this is to collect
-    # the results to a single rank to do the test.
-    if(P_world.rank == 0):
-        # Correct the norms from distributed calculation
-        global_results[:4] = np.sqrt(global_results[:4])
-
-        # Unpack the values
-        norm_x, norm_y, norm_Ax, norm_Asy, ip1, ip2 = global_results
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-    else:
-        # All other ranks pass the adjoint test
-        assert(True)
-
-    # Barrier fence to ensure all enclosed MPI calls resolve.
-    P_world.comm.Barrier()
-
-
-# No comm needed
-def test_average_pooling_layer_2d_3():
-
-    import numpy as np
-    import torch
-    from mpi4py import MPI
-
-    from distdl.backends.mpi.partition import MPIPartition
-    from distdl.nn.pooling import DistributedAvgPool2d
-    from distdl.utilities.slicing import compute_subsizes
-    from distdl.utilities.torch import NoneTensor
-
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
-
-    P = P_world.create_partition_inclusive(np.arange(4))
-    P_cart = P.create_cartesian_topology_partition([1, 1, 2, 2])
-
-    global_tensor_sizes = np.array([1, 1, 8, 8])
-
-    layer = DistributedAvgPool2d(global_tensor_sizes, P_cart, kernel_size=[2, 2])
-
-    x = NoneTensor()
-    if P_cart.active:
-        input_tensor_sizes = compute_subsizes(P_cart.dims, P_cart.cartesian_coordinates(P_cart.rank), global_tensor_sizes)
-        x = torch.tensor(np.random.randn(*input_tensor_sizes))
-    x.requires_grad = True
-
-    Ax = layer(x)
-
-    y = NoneTensor()
-    if P_cart.active:
-        y = torch.tensor(np.random.randn(*Ax.shape))
-    y.requires_grad = True
-
-    Ax.backward(y)
-    Asy = x.grad
-
-    x = x.detach()
-    Ax = Ax.detach()
-    y = y.detach()
-    Asy = Asy.detach()
-
-    local_results = np.zeros(6, dtype=np.float64)
-    global_results = np.zeros(6, dtype=np.float64)
-
-    # Compute all of the local norms and inner products.
-    # We only perform the inner product calculation between
-    # x and Asy on the root rank, as the input space of the forward
-    # operator and the output space of the adjoint operator
-    # are only relevant to the root rank
-    if P_cart.active:
-        # ||x||^2
-        local_results[0] = (torch.norm(x)**2).numpy()
-        # ||y||^2
-        local_results[1] = (torch.norm(y)**2).numpy()
-        # ||A@x||^2
-        local_results[2] = (torch.norm(Ax)**2).numpy()
-        # ||A*@y||^2
-        local_results[3] = (torch.norm(Asy)**2).numpy()
-        # <A@x, y>
-        local_results[4] = np.array([torch.sum(torch.mul(Ax, y))])
-        # <A*@y, x>
-        local_results[5] = np.array([torch.sum(torch.mul(Asy, x))])
-
-    # Reduce the norms and inner products
-    P_world.comm.Reduce(local_results, global_results, op=MPI.SUM, root=0)
-
-    # Because this is being computed in parallel, we risk that these norms
-    # and inner products are not exactly equal, because the floating point
-    # arithmetic is not commutative.  The only way to fix this is to collect
-    # the results to a single rank to do the test.
-    if(P_world.rank == 0):
-        # Correct the norms from distributed calculation
-        global_results[:4] = np.sqrt(global_results[:4])
-
-        # Unpack the values
-        norm_x, norm_y, norm_Ax, norm_Asy, ip1, ip2 = global_results
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-    else:
-        # All other ranks pass the adjoint test
-        assert(True)
-
-    # Barrier fence to ensure all enclosed MPI calls resolve.
-    P_world.comm.Barrier()
+    check_adjoint_test_tight(P_world, x, dx, y, dy)
