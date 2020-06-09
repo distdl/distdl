5124b840677b9fc52346205488917a6f36d00675
Russell J. Hewett <rhewett@vt.edu>
2020-06-09 09:50:44 -0400

    feat(#110) Created base module.

---
+++ src/distdl/nn/module.py
@@ -0,0 +1,31 @@
+import torch
+
+
+class Module(torch.nn.Module):
+
+    def __init__(self):
+
+        super(Module, self).__init__()
+
+        self._distdl_is_setup = False
+        self.register_forward_pre_hook(self._distdl_forward_pre_hook)
+
+    def _distdl_forward_pre_hook(self, input):
+
+        if self._distdl_module_requires_reset():
+
+            if self._distdl_is_setup:
+                self._distdl_module_teardown()
+
+            self._distdl_module_setup()
+
+        return
+
+    def _distdl_module_setup(self):
+        pass
+
+    def _distdl_module_teardown(self):
+        pass
+
+    def _distdl_module_requires_reset(self):
+        return (not self._distdl_is_setup) or True
