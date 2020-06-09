3924edde398dd66b9e223fae095c37adc4c0bf11
Russell J. Hewett <rhewett@vt.edu>
2020-06-09 15:04:58 -0400

    refactor(#109) Added function for assembling global tensor size on the fly.

--- src/distdl/backends/mpi/__init__.py
+++ src/distdl/backends/mpi/__init__.py
@@ -3,6 +3,9 @@ from . import compare  # noqa: F401
 from . import exchange_tensor  # noqa: F401
 from . import partition  # noqa: F401
 #
+#
+from .exchange_tensor import compute_global_tensor_sizes  # noqa: F401
+#
 # Expose the partition types
 from .partition import MPICartesianPartition as CartesianPartition  # noqa: F401
 from .partition import MPIPartition as Partition  # noqa: F401
