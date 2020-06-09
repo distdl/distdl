5656a4d2d6939e9843144b8960eef1a9d1586a59
Russell J. Hewett <rhewett@vt.edu>
2020-06-09 08:47:00 -0400

    refactor(#19) Fixed pad/unpad tests to use fixtures and parametrization.

--- tests/test_padnd_layer.py
+++ tests/test_padnd_layer.py
@@ -1,110 +1,76 @@
-# Tests padnd layer when each dimension has positive padding
-def test_padnd_layer_postive_padding():
+import pytest
+from adjoint_test import check_adjoint_test_tight
+
+adjoint_parametrizations = []
+
+adjoint_parametrizations.append(
+    pytest.param(
+        [3, 4, 5],  # tensor_sizes
+        [[1, 2], [1, 2], [1, 2]],  # pads
+        1,  # passed to comm_split_fixture, required MPI ranks
+        id="positive_padding",
+        marks=[pytest.mark.mpi(min_size=1)]
+        )
+    )
+
+adjoint_parametrizations.append(
+    pytest.param(
+        [3, 4, 5],  # tensor_sizes
+        [[1, 0], [0, 2], [0, 0]],  # pads
+        1,  # passed to comm_split_fixture, required MPI ranks
+        id="nonnegative_padding",
+        marks=[pytest.mark.mpi(min_size=1)]
+        )
+    )
+
+
+@pytest.mark.parametrize("tensor_sizes,"
+                         "pads,"
+                         "comm_split_fixture",
+                         adjoint_parametrizations,
+                         indirect=["comm_split_fixture"])
+def test_padnd_adjoint(barrier_fence_fixture,
+                       comm_split_fixture,
+                       tensor_sizes,
+                       pads):
 
     import numpy as np
     import torch
-    from mpi4py import MPI
 
     from distdl.backends.mpi.partition import MPIPartition
     from distdl.nn.padnd import PadNd
     from distdl.utilities.torch import NoneTensor
 
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
-    P = P_world.create_partition_inclusive(np.arange(1))
+    # Isolate the minimum needed ranks
+    base_comm, active = comm_split_fixture
+    if not active:
+        return
+    P_world = MPIPartition(base_comm)
+    P = P_world.create_partition_inclusive([0])
 
-    tensor_sizes = np.array([3, 4, 5])
-    pads = np.array([[1, 2], [1, 2], [1, 2]])
+    tensor_sizes = np.asarray(tensor_sizes)
+    pads = np.asarray(pads)
+
     padded_sizes = [t + lpad + rpad for t, (lpad, rpad) in zip(tensor_sizes, pads)]
 
-    padnd_layer = PadNd(pads, value=0, partition=P)
+    layer = PadNd(pads, value=0, partition=P)
 
     x = NoneTensor()
     if P.active:
         x = torch.tensor(np.random.randn(*tensor_sizes))
     x.requires_grad = True
 
-    y = NoneTensor()
+    dy = NoneTensor()
     if P.active:
-        y = torch.tensor(np.random.randn(*padded_sizes))
-    y.requires_grad = True
+        dy = torch.tensor(np.random.randn(*padded_sizes))
 
-    Ax = padnd_layer(x)
-    Ax.backward(y)
-    Asy = x.grad
+    y = layer(x)
+    y.backward(dy)
+    dx = x.grad
 
     x = x.detach()
-    Asy = Asy.detach()
+    dx = dx.detach()
+    dy = dy.detach()
     y = y.detach()
-    Ax = Ax.detach()
 
-    if P.active:
-        norm_x = np.sqrt((torch.norm(x)**2).numpy())
-        norm_y = np.sqrt((torch.norm(y)**2).numpy())
-        norm_Ax = np.sqrt((torch.norm(Ax)**2).numpy())
-        norm_Asy = np.sqrt((torch.norm(Asy)**2).numpy())
-        ip1 = np.array([torch.sum(torch.mul(Ax, y))])
-        ip2 = np.array([torch.sum(torch.mul(Asy, x))])
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-
-    else:
-        assert(True)
-
-
-# Tests padnd layer when each dimension has nonnegative padding
-def test_padnd_layer_nonnegative_padding():
-
-    import numpy as np
-    import torch
-    from mpi4py import MPI
-
-    from distdl.backends.mpi.partition import MPIPartition
-    from distdl.nn.padnd import PadNd
-    from distdl.utilities.torch import NoneTensor
-
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
-    P = P_world.create_partition_inclusive(np.arange(1))
-
-    tensor_sizes = np.array([3, 4, 5])
-    pads = np.array([[1, 0], [0, 2], [0, 0]])
-    padded_sizes = [t + lpad + rpad for t, (lpad, rpad) in zip(tensor_sizes, pads)]
-
-    padnd_layer = PadNd(pads, value=0, partition=P)
-
-    x = NoneTensor()
-    if P.active:
-        x = torch.tensor(np.random.randn(*tensor_sizes))
-    x.requires_grad = True
-
-    y = NoneTensor()
-    if P.active:
-        y = torch.tensor(np.random.randn(*padded_sizes))
-    y.requires_grad = True
-
-    Ax = padnd_layer(x)
-    Ax.backward(y)
-    Asy = x.grad
-
-    x = x.detach()
-    Asy = Asy.detach()
-    y = y.detach()
-    Ax = Ax.detach()
-
-    if P.active:
-        norm_x = np.sqrt((torch.norm(x)**2).numpy())
-        norm_y = np.sqrt((torch.norm(y)**2).numpy())
-        norm_Ax = np.sqrt((torch.norm(Ax)**2).numpy())
-        norm_Asy = np.sqrt((torch.norm(Asy)**2).numpy())
-        ip1 = np.array([torch.sum(torch.mul(Ax, y))])
-        ip2 = np.array([torch.sum(torch.mul(Asy, x))])
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-
-    else:
-        assert(True)
+    check_adjoint_test_tight(P_world, x, dx, y, dy)
--- tests/test_unpadnd_layer.py
+++ tests/test_unpadnd_layer.py
@@ -1,109 +1,76 @@
-def test_unpadnd_layer_postive_padding():
+import pytest
+from adjoint_test import check_adjoint_test_tight
+
+adjoint_parametrizations = []
+
+adjoint_parametrizations.append(
+    pytest.param(
+        [4, 5, 6],  # tensor_sizes
+        [[1, 2], [1, 2], [1, 2]],  # pads
+        1,  # passed to comm_split_fixture, required MPI ranks
+        id="positive_padding",
+        marks=[pytest.mark.mpi(min_size=1)]
+        )
+    )
+
+adjoint_parametrizations.append(
+    pytest.param(
+        [4, 5, 6],  # tensor_sizes
+        [[1, 0], [0, 2], [0, 0]],  # pads
+        1,  # passed to comm_split_fixture, required MPI ranks
+        id="nonnegative_padding",
+        marks=[pytest.mark.mpi(min_size=1)]
+        )
+    )
+
+
+@pytest.mark.parametrize("tensor_sizes,"
+                         "pads,"
+                         "comm_split_fixture",
+                         adjoint_parametrizations,
+                         indirect=["comm_split_fixture"])
+def test_unpadnd_adjoint(barrier_fence_fixture,
+                         comm_split_fixture,
+                         tensor_sizes,
+                         pads):
 
     import numpy as np
     import torch
-    from mpi4py import MPI
 
     from distdl.backends.mpi.partition import MPIPartition
     from distdl.nn.unpadnd import UnPadNd
     from distdl.utilities.torch import NoneTensor
 
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
-    P = P_world.create_partition_inclusive(np.arange(1))
+    # Isolate the minimum needed ranks
+    base_comm, active = comm_split_fixture
+    if not active:
+        return
+    P_world = MPIPartition(base_comm)
+    P = P_world.create_partition_inclusive([0])
 
-    tensor_sizes = np.array([4, 5, 6])
-    pads = np.array([[1, 2], [1, 2], [1, 2]])
+    tensor_sizes = np.asarray(tensor_sizes)
+    pads = np.asarray(pads)
 
-    unpadnd_layer = UnPadNd(pads, value=0, partition=P)
+    layer = UnPadNd(pads, value=0, partition=P)
 
     x = NoneTensor()
     if P.active:
         x = torch.tensor(np.random.randn(*tensor_sizes))
     x.requires_grad = True
 
-    Ax = unpadnd_layer(x)
+    y = layer(x)
+    print(x, y)
 
-    y = NoneTensor()
+    dy = NoneTensor()
     if P.active:
-        y = torch.tensor(np.random.randn(*Ax.shape))
-    y.requires_grad = True
+        dy = torch.tensor(np.random.randn(*y.shape))
 
-    Ax.backward(y)
-    Asy = x.grad
+    y.backward(dy)
+    dx = x.grad
 
     x = x.detach()
-    Asy = Asy.detach()
+    dx = dx.detach()
+    dy = dy.detach()
     y = y.detach()
-    Ax = Ax.detach()
 
-    if P.active:
-        norm_x = np.sqrt((torch.norm(x)**2).numpy())
-        norm_y = np.sqrt((torch.norm(y)**2).numpy())
-        norm_Ax = np.sqrt((torch.norm(Ax)**2).numpy())
-        norm_Asy = np.sqrt((torch.norm(Asy)**2).numpy())
-        ip1 = np.array([torch.sum(torch.mul(Ax, y))])
-        ip2 = np.array([torch.sum(torch.mul(Asy, x))])
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-
-    else:
-        assert(True)
-
-
-# Tests unpadnd layer when each dimension has nonnegative padding
-def test_unpadnd_layer_nonnegative_padding():
-
-    import numpy as np
-    import torch
-    from mpi4py import MPI
-
-    from distdl.backends.mpi.partition import MPIPartition
-    from distdl.nn.unpadnd import UnPadNd
-    from distdl.utilities.torch import NoneTensor
-
-    P_world = MPIPartition(MPI.COMM_WORLD)
-    P_world.comm.Barrier()
-    P = P_world.create_partition_inclusive(np.arange(1))
-
-    tensor_sizes = np.array([3, 4, 5])
-    pads = np.array([[1, 0], [0, 2], [0, 0]])
-
-    unpadnd_layer = UnPadNd(pads, value=0, partition=P)
-
-    x = NoneTensor()
-    if P.active:
-        x = torch.tensor(np.random.randn(*tensor_sizes))
-    x.requires_grad = True
-
-    Ax = unpadnd_layer(x)
-
-    y = NoneTensor()
-    if P.active:
-        y = torch.tensor(np.random.randn(*Ax.shape))
-    y.requires_grad = True
-
-    Ax.backward(y)
-    Asy = x.grad
-
-    x = x.detach()
-    Asy = Asy.detach()
-    y = y.detach()
-    Ax = Ax.detach()
-
-    if P.active:
-        norm_x = np.sqrt((torch.norm(x)**2).numpy())
-        norm_y = np.sqrt((torch.norm(y)**2).numpy())
-        norm_Ax = np.sqrt((torch.norm(Ax)**2).numpy())
-        norm_Asy = np.sqrt((torch.norm(Asy)**2).numpy())
-        ip1 = np.array([torch.sum(torch.mul(Ax, y))])
-        ip2 = np.array([torch.sum(torch.mul(Asy, x))])
-
-        d = np.max([norm_Ax*norm_y, norm_Asy*norm_x])
-        print(f"Adjoint test: {ip1/d} {ip2/d}")
-        assert(np.isclose(ip1/d, ip2/d))
-
-    else:
-        assert(True)
+    check_adjoint_test_tight(P_world, x, dx, y, dy)
