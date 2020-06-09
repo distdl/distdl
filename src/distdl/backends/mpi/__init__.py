08a3360f8f683d724a151959af927fa62b9f6851
Russell J. Hewett <rhewett@vt.edu>
2020-06-09 12:37:43 -0400

    refactor(#109) Exposed generic partition types.

--- src/distdl/backends/mpi/__init__.py
+++ src/distdl/backends/mpi/__init__.py
@@ -1,3 +1,7 @@
 from . import compare  # noqa: F401
 from . import exchange_tensor  # noqa: F401
 from . import partition  # noqa: F401
+#
+# Expose the partition types
+from .partition import MPICartesianPartition as CartesianPartition  # noqa: F401
+from .partition import MPIPartition as Partition  # noqa: F401
