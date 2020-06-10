460bface1831f007fab8ed9d9c3727d94a554f02
Russell J. Hewett <rhewett@vt.edu>
2020-06-10 13:24:15 -0400

    refactor(#101) Fixed Broadcast and SumReduce to use setup/teardown.

--- src/distdl/nn/sum_reduce.py
+++ src/distdl/nn/sum_reduce.py
@@ -19,23 +19,59 @@ class SumReduce(Module):
 
         self.identity = False
 
+        # Blank partitions
+        self.P_send = self._distdl_backend.Partition()
+        self.P_recv = self._distdl_backend.Partition()
+
+        # Variables for tracking input changes and buffer construction
+        self._distdl_is_setup = False
+        self._input_shape = None
+        self._input_requires_grad = None
+
         # The identity case is if the partitions are of size 1,
         # or they are the same partition and neither is tranposed,
         # or they are the same partition and both are transposed.
-        if P_in == P_out:
-            if P_in.size == 1:
+        if self.P_in == self.P_out:
+            if self.P_in.size == 1:
                 self.identity = True
-            elif (transpose_dest and transpose_src) or \
-                 (not transpose_dest and not transpose_src):
+            elif (self.transpose_dest and self.transpose_src) or \
+                 (not self.transpose_dest and not self.transpose_src):
                 self.identity = True
 
-        # We do the actual work if it is not an identity
+    def _distdl_module_setup(self, input):
+
+        # If it is not an identity, we need actual Partitions to do the work.
         if not self.identity:
-            reduce_partitions = P_in.create_reduction_partition_to(P_out,
-                                                                   transpose_src,
-                                                                   transpose_dest)
+            reduce_partitions = self.P_in.create_reduction_partition_to(self.P_out,
+                                                                        self.transpose_src,
+                                                                        self.transpose_dest)
             self.P_send = reduce_partitions[0]
             self.P_recv = reduce_partitions[1]
+
+        self._distdl_is_setup = True
+        self._input_shape = input[0].shape
+        self._input_requires_grad = input[0].requires_grad
+
+    def _distdl_module_teardown(self, input):
+
+        # Reset all of the buffers and communication objects
+        self.P_send = self._distdl_backend.Partition()
+        self.P_recv = self._distdl_backend.Partition()
+
+        # Reset any info about the input
+        self._distdl_is_setup = False
+        self._input_shape = None
+        self._input_requires_grad = None
+
+    def _distdl_input_changed(self, input):
+
+        if input[0].requires_grad != self._input_requires_grad:
+            return True
+
+        if input[0].shape != self._input_shape:
+            return True
+
+        return False
 
     def forward(self, input):
 
