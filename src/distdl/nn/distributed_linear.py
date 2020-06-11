27c7694428df6400b6ed1add5ecab9e4571fddf0
Russell J. Hewett <rhewett@vt.edu>
2020-06-11 12:43:18 -0400

    refactor(#101) Linear layer does not require tensor size.

--- src/distdl/nn/distributed_linear.py
+++ src/distdl/nn/distributed_linear.py
@@ -8,43 +8,41 @@ from distdl.utilities.slicing import compute_subsizes
 
 class DistributedLinear(Module):
 
-    def __init__(self, P_x, x_sizes, P_y, y_sizes, P_mul, bias=True):
+    def __init__(self, P_x, P_y, P_w, in_features, out_features, bias=True):
 
         super(DistributedLinear, self).__init__()
 
+        # P_x ~ 1 X P_fi
         self.P_x = P_x
-        self.x_sizes = x_sizes
+        # P_y ~ 1 X P_fo
         self.P_y = P_y
-        self.y_sizes = y_sizes
-
-        self.P_mul = P_mul
+        # P_w ~ P_fo X P_fi
+        self.P_w = P_w
 
         self.bias = bias
 
-        self.x_broadcast = Broadcast(self.P_x, self.P_mul)
-        if self.P_mul.active:
-            # x_subsizes is junk in the y dimension and vice versa
-            x_subsizes = compute_subsizes(P_mul.dims, P_mul.coords, x_sizes)
-            # Because of the way subsizes works, and because the y dimesion is
-            # transposed for the multiplication, we need to reverse the
-            # dimensions so that the structure of y matches.
-            y_subsizes = compute_subsizes(P_mul.dims[::-1], P_mul.coords[::-1], y_sizes)
+        self.x_broadcast = Broadcast(self.P_x, self.P_w)
+
+        if self.P_w.active:
+            local_in_features = compute_subsizes(P_w.dims[1], P_w.coords[1], in_features)
+            local_out_features = compute_subsizes(P_w.dims[0], P_w.coords[0], out_features)
             # On column 0, use the specified bias, otherwise no bias to
             # prevent double counting
-            bias = self.bias if (self.P_mul.coords[-1] == 0) else False
-            self.sublinear = torch.nn.Linear(x_subsizes[-1], y_subsizes[-1], bias=bias)
-        self.y_sum_reduce = SumReduce(self.P_mul, self.P_y, transpose_src=True)
+            bias = self.bias if (self.P_w.coords[-1] == 0) else False
+            self.sublinear = torch.nn.Linear(local_in_features[0], local_out_features[0], bias=bias)
+
+        self.y_sum_reduce = SumReduce(self.P_w, self.P_y, transpose_src=True)
 
     def forward(self, input):
 
-        if not (self.P_x.active or self.P_y.active or self.P_mul.active):
+        if not (self.P_x.active or self.P_y.active or self.P_w.active):
             return input.clone()
 
         # broadcast x down the columns
         x = self.x_broadcast(input)
 
         # apply the linear layer
-        if self.P_mul.active:
+        if self.P_w.active:
             x = self.sublinear(x)
 
         # reduce y across the rows
