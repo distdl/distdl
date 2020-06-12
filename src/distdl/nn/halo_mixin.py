82339149e61687f8688dc98d1600a198d487300c
Russell J. Hewett <rhewett@vt.edu>
2020-06-12 11:38:11 -0400

    refactor(#114) Improved clarity in variable names.

--- src/distdl/nn/halo_mixin.py
+++ src/distdl/nn/halo_mixin.py
@@ -7,7 +7,7 @@ from distdl.utilities.slicing import compute_subsizes
 class HaloMixin:
 
     def _compute_exchange_info(self,
-                               global_tensor_shape,
+                               x_global_shape,
                                kernel_size,
                                stride,
                                padding,
@@ -21,14 +21,14 @@ class HaloMixin:
 
         dim = len(partition_dims)
 
-        global_tensor_shape = np.atleast_1d(global_tensor_shape)
+        x_global_shape = np.atleast_1d(x_global_shape)
         kernel_size = np.atleast_1d(kernel_size)
         stride = np.atleast_1d(stride)
         padding = np.atleast_1d(padding)
         dilation = np.atleast_1d(dilation)
 
         def compute_lpad_length(array):
-            return len(global_tensor_shape) - len(array)
+            return len(x_global_shape) - len(array)
 
         kernel_size = np.pad(kernel_size,
                              pad_width=(compute_lpad_length(kernel_size), 0),
@@ -49,7 +49,7 @@ class HaloMixin:
 
         halo_sizes = self._compute_halo_sizes(partition_dims,
                                               partition_coords,
-                                              global_tensor_shape,
+                                              x_global_shape,
                                               kernel_size,
                                               stride,
                                               padding,
@@ -63,7 +63,7 @@ class HaloMixin:
             lcoords = [x - 1 if j == i else x for j, x in enumerate(partition_coords)]
             nhalo = self._compute_halo_sizes(partition_dims,
                                              lcoords,
-                                             global_tensor_shape,
+                                             x_global_shape,
                                              kernel_size,
                                              stride,
                                              padding,
@@ -76,7 +76,7 @@ class HaloMixin:
             rcoords = [x + 1 if j == i else x for j, x in enumerate(partition_coords)]
             nhalo = self._compute_halo_sizes(partition_dims,
                                              rcoords,
-                                             global_tensor_shape,
+                                             x_global_shape,
                                              kernel_size,
                                              stride,
                                              padding,
@@ -86,16 +86,16 @@ class HaloMixin:
             if(rcoords[i] < partition_dims[i]):
                 send_buffer_sizes[i, 1] = nhalo[i, 0]
 
-        local_input_tensor_shape = compute_subsizes(partition_dims, partition_coords, global_tensor_shape)
+        x_local_shape = compute_subsizes(partition_dims, partition_coords, x_global_shape)
         halo_sizes_with_negatives = self._compute_halo_sizes(partition_dims,
                                                              partition_coords,
-                                                             global_tensor_shape,
+                                                             x_global_shape,
                                                              kernel_size,
                                                              stride,
                                                              padding,
                                                              dilation,
                                                              require_nonnegative=False)
-        needed_ranges = self._compute_needed_ranges(local_input_tensor_shape, halo_sizes_with_negatives)
+        needed_ranges = self._compute_needed_ranges(x_local_shape, halo_sizes_with_negatives)
 
         halo_sizes = halo_sizes.astype(int)
         needed_ranges = needed_ranges.astype(int)
@@ -126,50 +126,53 @@ class HaloMixin:
     def _compute_halo_sizes(self,
                             dims,
                             coords,
-                            global_input_tensor_shape,
+                            x_global_shape,
                             kernel_size,
                             stride,
                             padding,
                             dilation,
                             require_nonnegative=True):
 
-        global_input_tensor_shape = np.asarray(global_input_tensor_shape)
+        x_global_shape = np.asarray(x_global_shape)
 
-        local_input_tensor_shape = compute_subsizes(dims, coords, global_input_tensor_shape)
-        local_input_tensor_starts = compute_starts(dims, coords, global_input_tensor_shape)
+        x_local_shape = compute_subsizes(dims, coords, x_global_shape)
+        x_local_start_index = compute_starts(dims, coords, x_global_shape)
 
         # formula from pytorch docs for maxpool
-        global_output_tensor_shape = self._compute_out_sizes(global_input_tensor_shape, kernel_size,
-                                                             stride, padding, dilation)
+        y_global_shape = self._compute_out_sizes(x_global_shape, kernel_size,
+                                                 stride, padding, dilation)
 
-        local_output_tensor_shape = compute_subsizes(dims, coords, global_output_tensor_shape)
-        local_output_tensor_starts = compute_starts(dims, coords, global_output_tensor_shape)
+        y_local_shape = compute_subsizes(dims, coords, y_global_shape)
+        y_local_start_index = compute_starts(dims, coords, y_global_shape)
 
-        local_ouput_tensor_left_index = local_output_tensor_starts
-        local_input_tensor_left_index_needed = self._compute_min_input_range(local_ouput_tensor_left_index,
-                                                                             kernel_size,
-                                                                             stride,
-                                                                             padding,
-                                                                             dilation)
+        y_local_left_global_index = y_local_start_index
+        x_local_left_global_index_needed = self._compute_min_input_range(y_local_left_global_index,
+                                                                         kernel_size,
+                                                                         stride,
+                                                                         padding,
+                                                                         dilation)
         # Clamp to the boundary
-        local_input_tensor_left_index_needed = np.maximum(np.zeros_like(global_input_tensor_shape), local_input_tensor_left_index_needed)
+        x_local_left_global_index_needed = np.maximum(np.zeros_like(x_global_shape),
+                                                      x_local_left_global_index_needed)
 
-        local_output_tensor_right_index = local_output_tensor_starts + local_output_tensor_shape - 1
-        local_input_tensor_right_index_needed = self._compute_max_input_range(local_output_tensor_right_index,
-                                                                              kernel_size,
-                                                                              stride,
-                                                                              padding,
-                                                                              dilation)
+        y_local_right_global_index = y_local_start_index + y_local_shape - 1
+        x_local_right_global_index_needed = self._compute_max_input_range(y_local_right_global_index,
+                                                                          kernel_size,
+                                                                          stride,
+                                                                          padding,
+                                                                          dilation)
         # Clamp to the boundary
-        local_input_tensor_right_index_needed = np.minimum(global_input_tensor_shape - 1, local_input_tensor_right_index_needed)
+        x_local_right_global_index_needed = np.minimum(x_global_shape - 1,
+                                                       x_local_right_global_index_needed)
 
         # Compute the actual ghost values
-        local_input_tensor_left_ghost = local_input_tensor_starts - local_input_tensor_left_index_needed
-        local_input_tensor_right_ghost = local_input_tensor_right_index_needed - (local_input_tensor_starts + local_input_tensor_shape - 1)
+        x_local_left_halo_shape = x_local_start_index - x_local_left_global_index_needed
+        x_local_stop_index = x_local_start_index + x_local_shape - 1
+        x_local_right_halo_shape = x_local_right_global_index_needed - x_local_stop_index
 
         # Make sure the halos are always positive, so we get valid buffer sizes
         if require_nonnegative:
-            local_input_tensor_left_ghost = np.maximum(local_input_tensor_left_ghost, 0)
-            local_input_tensor_right_ghost = np.maximum(local_input_tensor_right_ghost, 0)
+            x_local_left_halo_shape = np.maximum(x_local_left_halo_shape, 0)
+            x_local_right_halo_shape = np.maximum(x_local_right_halo_shape, 0)
 
-        return np.hstack([local_input_tensor_left_ghost, local_input_tensor_right_ghost]).reshape(2, -1).T
+        return np.hstack([x_local_left_halo_shape, x_local_right_halo_shape]).reshape(2, -1).T
