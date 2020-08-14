ef179984b0988f7b6d4c4f473cacfa841ead68dd
Russell J. Hewett <rjh@rjh.io>
2020-08-14 16:19:45 -0400

    feat(#17) Added pooling layer docs.

--- docs/code_reference/nn/pooling.rst
+++ docs/code_reference/nn/pooling.rst
@@ -2,51 +2,143 @@
 Pooling Layers
 ==============
 
+.. contents::
+    :local:
+    :depth: 2
+
 Overview
 ========
 
-.. automodule:: distdl.nn.pooling
+DistDL's The Distributed Pooling layers use the distributed primitive
+layers to build various distributed versions of PyTorch Pooling layers.
+
+For the purposes of this documentation, we will assume that an arbitrary
+global input tensor :math:`{x}` is partitioned by :math:`P_x`.
+
+
+Implementation
+==============
+
+Currently, all pooling operations follow the same pattern.  Therefore,
+a single base class implements the core distributed work and the
+actual pooling operation is deferred to the underlying PyTorch layer.
+
+As there are no learnable parameters in these layers, the parallelism is
+induced by partitions of the inout (and therefore output) tensors. Here, input
+(and outout) tensors that are distributed in feature-space only.
+
+Construction of this layer is driven by the partitioning of the input tensor
+:math:`x`, only.  Thus, the partition :math:`P_x` drives the algorithm design.
+With a pure feature-space partition, the output partition will have the same
+structure, so there is no need to specify it.
+
+In general, due to the non-centered nature of pooling kernels, halos will
+be one-sided.  See the `motivating paper <https://arxiv.org/abs/2006.03108>`_
+for more details.
+
+Assumptions
+-----------
+
+* The global input tensor :math:`x` has shape :math:`n_{\text{b}} \times
+  n_{c_{\text{in}}} \times n_{D-1} \times \cdots \times n_0`.
+* The input partition :math:`P_x` has shape :math:`1 \times 1 \times P_{D-1}
+  \times \cdots \times P_0`, where :math:`P_{d}` is the number of workers
+  partitioning the :math:`d^{\text{th}}` feature dimension of :math:`x`.
+* The global output tensor :math:`y` will have shape :math:`n_{\text{b}}
+  \times n_{c_{\text{out}}} \times m_{D-1} \times \cdots \times m_0`.
+  The precise values of :math:`m_{D-1} \times \cdots \times m_0` are
+  dependent on the input shape and the kernel parameters.
+* The output partition :math:`P_y` implicitly has the same shape as
+  :math:`P_x`.
+
+
+Forward
+~~~~~~~
+
+Under the above assumptions, the forward algorithm is:
+
+1. Perform the halo exchange on the subtensors of :math:`x`.  Here, :math:`x_j`
+   must be padded to accept local halo regions (in a potentially unbalanced
+   way) before the halos are exchanged.  The output of this operation is
+   :math:`\hat x_j`.
+
+2. Perform the local forward pooling application using a PyTorch
+   pooling layer.  The bias is added everywhere, as each workers output
+   will be part of the output tensor.
+
+
+Adjoint
+~~~~~~~
+
+The adjoint algorithm is not explicitly implemented.  PyTorch's ``autograd``
+feature automatically builds the adjoint of the Jacobian of the
+feature-distributed convolution forward application.  Essentially, the
+algorithm is as follows:
+
+
+1. Each worker computes its local contribution to :math:`\delta x`,
+   given by :math:`\delta x_j`, using PyTorch's native implementation of
+   the adjoint of the Jacobian of the local sequential pooling layer.
+
+2. The adjoint of the halo exchange is applied to :math:`\delta \hat x`,
+   which is then unpadded, producing the gradient input :math:`\delta x`.
 
 Pooling Mixin
 -------------
 
-.. automodule:: distdl.nn.mixins.pooling_mixin
+Some distributed pooling layers require more than their local subtensor to
+compute the correct local output.  This is governed by the "left" and "right"
+extent of the pooling window.  As these calculations are the same for all
+pooling operations, they are mixed in to every pooling layer requiring a halo
+exchange.
+
+Assumptions
+~~~~~~~~~~~
+
+* Pooling kernels are not centered, the origin of the window is the "upper left"
+  entry.
+* When a kernel has even size, the left side of the kernel is the shorter side.
+
+.. warning::
+   Current calculations of the subtensor index ranges required do not correctly
+   take padding and dilation into account.
+
+
+Examples
+========
 
 API
 ===
 
+
+``distdl.nn.pooling``
+--------------------------
+
 .. currentmodule:: distdl.nn.pooling
 
-.. autoclass:: DistributedPoolBase
+.. autoclass:: DistributedPoolingBase
     :members:
-    :exclude-members: forward
 
 .. autoclass:: DistributedAvgPool1d
     :members:
-    :exclude-members: forward
 
 .. autoclass:: DistributedAvgPool2d
     :members:
-    :exclude-members: forward
 
 .. autoclass:: DistributedAvgPool3d
     :members:
-    :exclude-members: forward
 
 .. autoclass:: DistributedMaxPool1d
     :members:
-    :exclude-members: forward
 
 .. autoclass:: DistributedMaxPool2d
     :members:
-    :exclude-members: forward
 
 .. autoclass:: DistributedMaxPool3d
     :members:
-    :exclude-members: forward
-
 
 .. currentmodule:: distdl.nn.mixins
 
 .. autoclass:: PoolingMixin
     :members:
+    :private-members:
--- src/distdl/nn/pooling.py
+++ src/distdl/nn/pooling.py
@@ -9,23 +9,57 @@ from distdl.utilities.slicing import assemble_slices
 
 
 class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
+    r"""A feature-space partitioned distributed pooling layer.
 
+    This class provides the user interface to a distributed pooling
+    layer, where the input (and output) tensors are partitioned in
+    feature-space only.
+
+    The base unit of work is given by the input/output tensor partition.  This
+    class requires the following of the tensor partitions:
+
+    1. :math:`P_x` over input tensor :math:`x` has shape :math:`1 \times
+       P_{\text{c_in}} \times 1 \times \dots \times 1`.
+
+    The output partition, :math:`P_y`, is assumed to be the same as the
+    input partition.
+
+    The first dimension of the input/output partitions is the batch
+    dimension,the second is the channel dimension, and remaining dimensions
+    are feature dimensions.
+
+    There are no learnable parameters.
+
+    All inputs to this base class are passed through to the underlying PyTorch
+    pooling layer.
+
+    Parameters
+    ----------
+    P_x :
+        Partition of input tensor.
+
+    """
+
+    # Pooling class for base unit of work.
     TorchPoolType = None  # noqa F821
 
     def __init__(self, P_x, *args, **kwargs):
 
         super(DistributedPoolBase, self).__init__()
 
+        # P_x is 1 x 1 x P_d-1 x ... x P_0
         self.P_x = P_x
 
         if not self.P_x.active:
             return
 
+        # Do this before checking serial so that the layer works properly
+        # in the serial case
         self.pool_layer = self.TorchPoolType(*args, **kwargs)
 
-        # We need the halo shape, and other info, to fully populate the pad,
-        # and halo exchange layers.
-
+        # We need the halo shape, and other info, to fully populate the pad
+        # and halo exchange layers.  For pad, we defer the construction to the
+        # pre-forward hook.
         self.pad_layer = None
 
         # We need to be able to remove some data from the input to the conv
@@ -36,7 +70,6 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
         # the halo shape for the input.  The halo will allocate its own
         # buffers, but it needs this information at construction to be able
         # to do this in the pre-forward hook.
-
         self.halo_layer = None
 
         # Variables for tracking input changes and buffer construction
@@ -45,6 +78,18 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
         self._input_requires_grad = None
 
     def _distdl_module_setup(self, input):
+        r"""Distributed (feature) pooling module setup function.
+
+        This function is called every time something changes in the input
+        tensor structure.  It should not be called manually.
+
+        Parameters
+        ----------
+        input :
+            Tuple of forward inputs.  See
+            `torch.nn.Module.register_forward_pre_hook` for more details.
+
+        """
 
         self._distdl_is_setup = True
         self._input_shape = input[0].shape
@@ -53,10 +98,12 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
         if not self.P_x.active:
             return
 
+        # To compute the halo regions, we need the global tensor shape.  This
+        # is not available until when the input is provided.
         x_global_shape = self._distdl_backend.compute_global_tensor_shape(input[0],
                                                                           self.P_x)
-        self.x_global_shape = x_global_shape
 
+        # Using that information, we can get there rest of the halo information
         exchange_info = self._compute_exchange_info(x_global_shape,
                                                     self.pool_layer.kernel_size,
                                                     self.pool_layer.stride,
@@ -79,18 +126,29 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
                                        recv_buffer_shape,
                                        send_buffer_shape)
 
-        # We have to select out the "unused" entries.
+        # We have to select out the "unused" entries.  Sometimes there can
+        # be "negative" halos.
         self.needed_slices = assemble_slices(needed_ranges[:, 0],
                                              needed_ranges[:, 1])
 
     def _distdl_module_teardown(self, input):
+        r"""Distributed (channel) pooling module teardown function.
+
+        This function is called every time something changes in the input
+        tensor structure.  It should not be called manually.
+
+        Parameters
+        ----------
+        input :
+            Tuple of forward inputs.  See
+            `torch.nn.Module.register_forward_pre_hook` for more details.
+
+        """
 
         # Reset all sub_layers
         self.pad_layer = None
         self.needed_slices = None
         self.halo_layer = None
-
-        self.x_global_shape = None
 
         # Reset any info about the input
         self._distdl_is_setup = False
@@ -98,6 +156,15 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
         self._input_requires_grad = None
 
     def _distdl_input_changed(self, input):
+        r"""Determine if the structure of inputs has changed.
+
+        Parameters
+        ----------
+        input :
+            Tuple of forward inputs.  See
+            `torch.nn.Module.register_forward_pre_hook` for more details.
+
+        """
 
         if input[0].requires_grad != self._input_requires_grad:
             return True
@@ -108,6 +175,14 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
         return False
 
     def forward(self, input):
+        r"""Forward function interface.
+
+        Parameters
+        ----------
+        input :
+            Input tensor to be broadcast.
+
+        """
 
         if not self.P_x.active:
             return input.clone()
@@ -119,30 +194,48 @@ class DistributedPoolBase(Module, HaloMixin, PoolingMixin):
 
 
 class DistributedAvgPool1d(DistributedPoolBase):
+    r"""A feature-partitioned distributed 1d average pooling layer.
+
+    """
 
     TorchPoolType = torch.nn.AvgPool1d
 
 
 class DistributedAvgPool2d(DistributedPoolBase):
+    r"""A feature-partitioned distributed 2d average pooling layer.
+
+    """
 
     TorchPoolType = torch.nn.AvgPool2d
 
 
 class DistributedAvgPool3d(DistributedPoolBase):
+    r"""A feature-partitioned distributed 3d average pooling layer.
+
+    """
 
     TorchPoolType = torch.nn.AvgPool3d
 
 
 class DistributedMaxPool1d(DistributedPoolBase):
+    r"""A feature-partitioned distributed 1d max pooling layer.
+
+    """
 
     TorchPoolType = torch.nn.MaxPool1d
 
 
 class DistributedMaxPool2d(DistributedPoolBase):
+    r"""A feature-partitioned distributed 2d max pooling layer.
+
+    """
 
     TorchPoolType = torch.nn.MaxPool2d
 
 
 class DistributedMaxPool3d(DistributedPoolBase):
+    r"""A feature-partitioned distributed 3d max pooling layer.
+
+    """
 
     TorchPoolType = torch.nn.MaxPool3d
