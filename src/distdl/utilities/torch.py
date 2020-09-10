36fc59c596f54c520b0eac654e03e618913ebae6
Russell J. Hewett <rhewett@vt.edu>
2020-09-10 16:32:04 -0400

    refactor(#75) Added wrapper for basic tensor structure.

--- src/distdl/utilities/torch.py
+++ src/distdl/utilities/torch.py
@@ -7,3 +7,30 @@ def zero_volume_tensor(b=None):
         return torch.empty((0,))
 
     return torch.empty((b, 0))
+
+
+class TensorStructure:
+    """ Light-weight class to store and compare basic structure of Torch tensors.
+
+    """
+
+    def __init__(self, tensor=None):
+
+        self.shape = None
+        self.dtype = None
+        self.requires_grad = None
+
+        if tensor is not None:
+            self.fill_from_tensor(tensor)
+
+    def fill_from_tensor(self, tensor):
+
+        self.shape = tensor.shape
+        self.dtype = tensor.dtype
+        self.requires_grad = tensor.requires_grad
+
+    def __eq__(self, other):
+
+        return ((self.shape == other.shape) and
+                (self.dtype == other.dtype) and
+                (self.requires_grad == other.requires_grad))
