c1e4481b31d1ca40fbf68bbbf42f08260395bef3
Russell J. Hewett <rhewett@vt.edu>
2020-12-01 10:30:31 -0500

    chore(#184) Release v0.3.0.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ project = 'DistDL'
 year = '2020'
 author = 'Russell J. Hewett'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.0'
+version = release = '0.3.0'
 
 pygments_style = 'trac'
 # templates_path = ['.']
