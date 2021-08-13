57da8a5176aaf299e72b3175e6b6d8103d7bfde0
Jacob Merizian <jmerizia@vt.edu>
2021-08-13 19:56:04 -0400

    fix run script and add all tests

--- run_gpu_tests.sh
+++ run_gpu_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --nodes 4
-#SBATCH --ntasks-per-node=5
+#SBATCH --nodes 2
+#SBATCH --ntasks-per-node=10
 #SBATCH --time 00:10:00
 #SBATCH --partition t4_normal_q
 #SBATCH --account distdl
@@ -12,19 +12,8 @@
 cd $SLURM_SUBMIT_DIR
 
 # Load modules silently
-{
-    module load mpi4py/3.0.2-gompi-2020a-timed-pingpong
-    module load OpenMPI/4.0.5-gcccuda-2020b
-    module load PyTorch/1.7.1-fosscuda-2020b
-} &> /dev/null
+module load mpi4py/3.0.2-gompi-2020a-timed-pingpong
+module load OpenMPI/4.0.5-gcccuda-2020b
+module load PyTorch/1.7.1-fosscuda-2020b
 
-mpirun --n 20 -x USE_CUDA=1 python -B -m mpi4py -m pytest --with-mpi -rsa -x \
-    tests/test_transpose.py \
-    tests/test_halo_exchange.py \
-    tests/test_sum_reduce.py \
-    tests/test_broadcast.py \
-    tests/test_padnd.py \
-    tests/test_conv_feature.py \
-    tests/test_batchnorm.py \
-    tests/test_loss.py \
-    tests/test_pooling.py
+mpirun --n 20 -x USE_CUDA=1 python -B -m mpi4py -m pytest --with-mpi -rsa -x tests
