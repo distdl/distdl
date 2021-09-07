64d55ef33ea4eeabcfd78c99c4c41e1cdc481ebe
Russell J. Hewett <rhewett@vt.edu>
2021-09-07 13:48:03 -0400

    refactor(#221) Updated transpose examples.

--- examples/primitives/ex_transpose.py
+++ examples/primitives/ex_transpose.py
@@ -1,3 +1,10 @@
+# This example demonstrates the behavior of the transpose primitive.
+#
+# It requires 4 workers to run.
+#
+# Run with, e.g.,
+#     > mpirun -np 4 python ex_transpose.py
+
 import numpy as np
 import torch
 from mpi4py import MPI
@@ -5,29 +12,48 @@ from mpi4py import MPI
 import distdl.utilities.slicing as slicing
 from distdl.backends.mpi.partition import MPIPartition
 from distdl.nn.transpose import DistributedTranspose
-from distdl.utilities.debug import print_sequential
 from distdl.utilities.torch import zero_volume_tensor
 
 # Set up MPI cartesian communicator
-
 P_world = MPIPartition(MPI.COMM_WORLD)
 P_world._comm.Barrier()
 
+# Create the input partition (using the first 4 workers)
 in_shape = (4, 1)
-out_shape = (2, 2)
 in_size = np.prod(in_shape)
-out_size = np.prod(out_shape)
+in_workers = np.arange(0, in_size)
 
-P_x_base = P_world.create_partition_inclusive(np.arange(0, in_size))
+P_x_base = P_world.create_partition_inclusive(in_workers)
 P_x = P_x_base.create_cartesian_topology_partition(in_shape)
 
-P_y_base = P_world.create_partition_inclusive(np.arange(P_world.size-out_size, P_world.size))
+# Create the output partition (using the last 4 workers)
+out_shape = (2, 2)
+out_size = np.prod(out_shape)
+out_workers = np.arange(P_world.size-out_size, P_world.size)
+
+P_y_base = P_world.create_partition_inclusive(out_workers)
 P_y = P_y_base.create_cartesian_topology_partition(out_shape)
 
+# This global tensor shape is among the smallest useful shapes for an example
 x_global_shape = np.array([7, 5])
 
+# Create the transpose layer
 layer = DistributedTranspose(P_x, P_y, preserve_batch=False)
 
+# Setup the input tensor.  Any worker in P_x will generate its part of the
+# input tensor.  Any worker not in P_x will have a zero-volume tensor.
+#
+# Input tensor will be (on a 4 x 1 partition):
+# [ [ 1 1 1 1 1 ]
+#   [ 1 1 1 1 1 ]
+#   -------------
+#   [ 2 2 2 2 2 ]
+#   [ 2 2 2 2 2 ]
+#   -------------
+#   [ 3 3 3 3 3 ]
+#   [ 3 3 3 3 3 ]
+#   -------------
+#   [ 4 4 4 4 4 ] ]
 x = zero_volume_tensor()
 if P_x.active:
     x_local_shape = slicing.compute_subshape(P_x.shape,
@@ -36,11 +62,35 @@ if P_x.active:
     x = np.zeros(x_local_shape) + P_x.rank + 1
     x = torch.from_numpy(x)
 x.requires_grad = True
-print_sequential(P_world._comm, f"x_{P_world.rank}: {x}")
+print(f"rank {P_world.rank}; index {P_x.index}; value {x}")
 
+# Apply the layer.
+#
+# Output tensor will be (on a 2 x 2 partition):
+# [ [ 1 1 1 | 1 1 ]
+#   [ 1 1 1 | 1 1 ]
+#   [ 2 2 2 | 2 2 ]
+#   [ 2 2 2 | 2 2 ]
+#   -------------
+#   [ 3 3 3 | 3 3 ]
+#   [ 3 3 3 | 3 3 ]
+#   [ 4 4 4 | 4 4 ] ]
 y = layer(x)
-print_sequential(P_world._comm, f"y_{P_world.rank}: {y}")
+print(f"rank {P_world.rank}; index {P_y.index}; value {y}")
 
+# Setup the adjoint input tensor.  Any worker in P_y will generate its part of
+# the adjoint input tensor.  Any worker not in P_y will have a zero-volume
+# tensor.
+#
+# Adjoint input tensor will be (on a 2 x 2 partition):
+# [ [ 1 1 1 | 2 2 ]
+#   [ 1 1 1 | 2 2 ]
+#   [ 1 1 1 | 2 2 ]
+#   [ 1 1 1 | 2 2 ]
+#   -------------
+#   [ 3 3 3 | 4 4 ]
+#   [ 3 3 3 | 4 4 ]
+#   [ 3 3 3 | 4 4 ] ]
 dy = zero_volume_tensor()
 if P_y.active:
     y_local_shape = slicing.compute_subshape(P_y.shape,
@@ -48,9 +98,21 @@ if P_y.active:
                                              x_global_shape)
     dy = np.zeros(y_local_shape) + P_y.rank + 1
     dy = torch.from_numpy(dy)
+print(f"rank {P_world.rank}; index {P_y.index}; value {dy}")
 
-print_sequential(P_world._comm, f"dy_{P_world.rank}: {dy}")
-
+# Apply the adjoint of the layer.
+#
+# Adjoint output tensor will be (on a 4 x 1 partition):
+# [ [ 1 1 1 2 2 ]
+#   [ 1 1 1 2 2 ]
+#   -------------
+#   [ 1 1 1 2 2 ]
+#   [ 1 1 1 2 2 ]
+#   -------------
+#   [ 3 3 3 4 4 ]
+#   [ 3 3 3 4 4 ]
+#   -------------
+#   [ 3 3 3 4 4 ] ]
 y.backward(dy)
 dx = x.grad
-print_sequential(P_world._comm, f"dx_{P_world.rank}: {dx}")
+print(f"rank {P_world.rank}; index {P_x.index}; value {dx}")
